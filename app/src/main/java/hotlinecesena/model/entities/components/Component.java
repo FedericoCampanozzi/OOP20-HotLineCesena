0cc516984086b485a6152a76e181ef2d4f0273b1
Remisse <zeromuseg7@gmail.com>
2021-03-16 19:45:04 +0100

    Add Javadoc to Component

--- app/src/main/java/hotlinecesena/model/entities/components/Component.java
+++ app/src/main/java/hotlinecesena/model/entities/components/Component.java
@@ -4,8 +4,14 @@ package hotlinecesena.model.entities.components;
  * <p>
  * Heavily inspired by https://github.com/divotkey/ecs
  * </p>
- * 
- * 
+ * <p>
+ * Encapsulates logic pertaining to a specific subset of the actors' possible actions.
+ * <br>
+ * In our case, components will store data and methods relative to movement, combat, etc.
+ * </p>
+ * <p>
+ * When creating an interface for a new component, extend this one.
+ * </p>
  */
 public interface Component {
 
