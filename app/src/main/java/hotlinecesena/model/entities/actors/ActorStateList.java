8c8cbab4d97cd53b8142cb58edfcd7fdad44ae56
Remisse <zeromuseg7@gmail.com>
2021-03-16 20:00:17 +0100

    Add Javadoc

--- app/src/main/java/hotlinecesena/model/entities/actors/ActorStateList.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/ActorStateList.java
@@ -2,10 +2,28 @@ package hotlinecesena.model.entities.actors;
 
 import hotlinecesena.model.entities.State;
 
+/**
+ * 
+ */
 public enum ActorStateList implements State {
 
+	/**
+	 * Actor is receiving no input.
+	 */
     IDLE,
+    
+    /**
+	 * Actor is moving.
+	 */
     MOVING,
+    
+    /**
+	 * Actor is attacking.
+	 */
     ATTACKING,
+    
+    /**
+	 * Actor is dead. All subsequent inputs should be ignored.
+	 */
     DEAD;
 }
