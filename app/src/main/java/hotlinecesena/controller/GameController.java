fa86df9687fadaf5b799edb3ee682a94a7e2773e
unknown <federico.campanozzi@studio.unibo.it>
2021-03-18 14:01:23 +0100

    fix project structure and complete DAL

--- app/src/main/java/hotlinecesena/controller/GameController.java
+++ app/src/main/java/hotlinecesena/controller/GameController.java
@@ -1,20 +1,17 @@
-package hotlinecesena.controller;
+package controller;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class GameController extends Application {
-    @Override
-    public void start(final Stage stage) {
 
-    }
+	public static void main(String[] args) {
+		
+	}
 
-    public static void main(final String[] args) {
-        launch(args);
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 
-    public Object getGreeting() {
-        // TODO Auto-generated method stub
-        return null;
-    }
+	}
+
 }
