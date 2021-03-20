ef92b49d29874dfc3ef09a3e885d7bf9c20bf90b
Remisse <zeromuseg7@gmail.com>
2021-03-20 15:02:03 +0100

    Remove setPosition(), add Javadoc

--- app/src/main/java/hotlinecesena/model/entities/Entity.java
+++ app/src/main/java/hotlinecesena/model/entities/Entity.java
@@ -2,10 +2,16 @@ package hotlinecesena.model.entities;
 
 import javafx.geometry.Point2D;
 
+/**
+ * 
+ * Represents a basic game entity to which a set of coordinates is associated.
+ *
+ */
 public interface Entity {
     
+    /**
+     * 
+     * @return this entity's coordinates on the game map.
+     */
     Point2D getPosition();
-    
-    void setPosition(Point2D pos);
-
 }
