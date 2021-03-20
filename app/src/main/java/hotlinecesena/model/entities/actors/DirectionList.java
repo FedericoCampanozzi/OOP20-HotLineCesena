ef344145b12331d8fb9f45d16d4362b7b29fec7b
Remisse <zeromuseg7@gmail.com>
2021-03-20 15:09:45 +0100

    Add Direction interface and its enum implementation

---
+++ app/src/main/java/hotlinecesena/model/entities/actors/DirectionList.java
@@ -0,0 +1,21 @@
+package hotlinecesena.model.entities.actors;
+
+import javafx.geometry.Point2D;
+
+public enum DirectionList implements Direction {
+    NORTH(new Point2D(0, -1)),
+    SOUTH(new Point2D(0, 1)),
+    EAST(new Point2D(1, 0)),
+    WEST(new Point2D(-1, 0));
+
+    private Point2D dir;
+    
+    DirectionList(final Point2D dir) {
+        this.dir = dir;
+    }
+    @Override
+    public Point2D get() {
+        return this.dir;
+    }
+
+}
