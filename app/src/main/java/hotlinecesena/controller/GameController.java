0255dc41694e09dc004cfe37638e499b8e65ed73
unknown <federico.campanozzi@gmail.com>
2021-03-22 00:53:50 +0100

    first tested game loop

--- app/src/main/java/hotlinecesena/controller/GameController.java
+++ app/src/main/java/hotlinecesena/controller/GameController.java
@@ -12,7 +12,8 @@ public class GameController extends Application {
 	
 	private static final int SCENE_WIDTH = 600;
     private static final int SCENE_HEIGHT = 400;
-    private static Stage stg;
+    private Stage stg;
+    private final GameLoopController glc = new GameLoopController();
     
 	public static void main(String[] args) throws IOException {
 		DALImpl.getInstance();
@@ -22,23 +23,25 @@ public class GameController extends Application {
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		stg = primaryStage;
-		
         Parent root = FXMLLoader.load(ClassLoader.getSystemResource(DALImpl.getInstance().getGuiPath().get("StartMenuView.fxml")));
-
-        // Stage configuration
         primaryStage.setTitle("HotLine Cesena");
         primaryStage.setScene(new Scene(root, SCENE_WIDTH, SCENE_HEIGHT));
         primaryStage.setResizable(false);
         primaryStage.show();
+        glc.loop();
+        glc.addMethodToUpdate((t) -> this.testMethodLoop(t) );
     }
 	
-    public Object getGreeting() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
 	public void changeScene(String fxml) throws IOException{
-		Parent pane = FXMLLoader.load(ClassLoader.getSystemResource(fxml));
+		Parent pane = FXMLLoader.load(ClassLoader.getSystemResource(DALImpl.getInstance().getGuiPath().get(fxml)));
 		stg.getScene().setRoot(pane);
+	}
+	
+	public void testMethodLoop(double deltaTime) {
+		System.out.println("loop example : " + deltaTime);
+	}
+	
+	public GameLoopController getGameLoop() {
+		return this.glc;
 	}
 }
