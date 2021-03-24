f43829e4f330a1fbf2e1e2654d196736108e875a
Remisse <zeromuseg7@gmail.com>
2021-03-24 22:36:53 +0100

    Refactor to include new state mechanism

--- app/src/main/java/hotlinecesena/model/entities/actors/player/PlayerImpl.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/player/PlayerImpl.java
@@ -1,44 +1,47 @@
 package hotlinecesena.model.entities.actors.player;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 import hotlinecesena.model.entities.actors.AbstractActor;
-import hotlinecesena.model.entities.actors.ActorStateList;
+import hotlinecesena.model.entities.actors.ActorStatus;
 import hotlinecesena.model.entities.actors.state.State;
+import hotlinecesena.model.entities.items.Item;
 import hotlinecesena.model.inventory.Inventory;
 import javafx.geometry.Point2D;
 import javafx.util.Pair;
 
 public class PlayerImpl extends AbstractActor implements Player {
 
-    private final Map<ActorStateList, Double> noiseLevels;
-    private final Map<ActorStateList, State> stateMap = Map.of(
-            ActorStateList.NORMAL, new PlayerNormalState(this),
-            ActorStateList.RELOADING, new PlayerReloadingState(this),
-            ActorStateList.DEAD, new PlayerDeadState(this)
+    private final Map<ActorStatus, Double> noiseLevels;
+    // Flyweight?
+    private final Map<ActorStatus, State> stateMap = Map.of(
+            ActorStatus.NORMAL, new PlayerNormalState(this),
+            ActorStatus.RELOADING, new PlayerReloadingState(this),
+            ActorStatus.DEAD, new PlayerDeadState(this)
             );
 
     public PlayerImpl(final Point2D pos, final double angle, final double speed,
-            final double maxHealth, final Inventory inv, final Map<ActorStateList, Double> noise) {
+            final double maxHealth, final Inventory inv, final Map<ActorStatus, Double> noise) {
         super(pos, angle, speed, maxHealth, inv);
         this.noiseLevels = noise;
     }
 
     @Override
     public double getNoiseRadius() {
-        return this.noiseLevels.get(this.getState());
+        return this.noiseLevels.get(this.getActorStatus());
     }
 
     @Override
     public void pickUp() {
-        //TODO Use DAL and collisions to determine which item, if there are any, will be picked up
+        //TODO Use DAL and collisions to determine which item will be picked up
         this.getInventory().add(null);
     }
 
     @Override
     public void use() {
-        final var item = this.getInventory().getUsable();
+        final Optional<Item> item = this.getInventory().getUsable();
         if (item.isPresent() && item.get().usage().isPresent()) {
             item.get().usage().get().accept(this);
         }
@@ -56,6 +59,7 @@ public class PlayerImpl extends AbstractActor implements Player {
 
     @Override
     public void handleCurrentState(Pair<Set<CommandType>, Point2D> commandsToHandle, double timeElapsed) {
-        this.stateMap.get(this.getState()).handle(commandsToHandle, timeElapsed);
+        this.stateMap.getOrDefault(this.getActorStatus(), this.stateMap.get(ActorStatus.NORMAL))
+            .handle(commandsToHandle, timeElapsed);
     }
 }
