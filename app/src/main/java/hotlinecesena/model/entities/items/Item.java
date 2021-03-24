87e74e737bf05c41457c988c384ae4110d778aad
Remisse <zeromuseg7@gmail.com>
2021-03-24 20:18:48 +0100

    Refactor for new item design

--- app/src/main/java/hotlinecesena/model/entities/items/Item.java
+++ app/src/main/java/hotlinecesena/model/entities/items/Item.java
@@ -1,5 +1,15 @@
 package hotlinecesena.model.entities.items;
 
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import hotlinecesena.model.entities.actors.Actor;
+
 public interface Item {
 
+    Optional<Consumer<Actor>> usage();
+    
+    Optional<Item> getReloadItem();
+    
+    int getMaxStacks();
 }
