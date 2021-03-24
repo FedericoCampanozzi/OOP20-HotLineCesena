15e678f1289c0f73f8e540967a97c7d394ab14e4
Remisse <zeromuseg7@gmail.com>
2021-03-24 20:20:16 +0100

    List of commands for stronger separation between M and C

--- app/src/main/java/hotlinecesena/model/entities/actors/player/CommandType.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/player/CommandType.java
@@ -1,27 +1,13 @@
 package hotlinecesena.model.entities.actors.player;
 
-import hotlinecesena.model.entities.actors.DirectionList;
-
 public enum CommandType {
 
-    MOVE_DIAGONAL((p, dir, ang) -> p.move(dir)),
-    MOVE_NORTH((p, dir, ang) -> p.move(DirectionList.NORTH.get())),
-    MOVE_SOUTH((p, dir, ang) -> p.move(DirectionList.SOUTH.get())),
-    MOVE_EAST((p, dir, ang) -> p.move(DirectionList.EAST.get())),
-    MOVE_WEST((p, dir, ang) -> p.move(DirectionList.WEST.get())),
-    ROTATE((p, dir, ang) -> p.setAngle(ang)),
-    ATTACK((p, dir, ang) -> p.attack()),
-    RELOAD((p, dir, ang) -> p.reload()),
-    PICK_UP((p, dir, ang) -> p.pickUp()),
-    USE((p, dir, ang) -> p.useItem());
-    
-    private Command<Player> command;
-    
-    CommandType(final Command<Player> command) {
-        this.command = command;
-    }
-    
-    public Command<Player> getCommand() {
-        return this.command;
-    }
+    MOVE_NORTH,
+    MOVE_SOUTH,
+    MOVE_EAST,
+    MOVE_WEST,
+    ATTACK,
+    RELOAD,
+    PICK_UP,
+    USE;
 }
