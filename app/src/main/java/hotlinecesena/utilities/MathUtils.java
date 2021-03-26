d183e799f3f2ff84e70276206aad1a57d20784f1
Remisse <zeromuseg7@gmail.com>
2021-03-26 17:56:16 +0100

    Add temporary utility class

---
+++ app/src/main/java/hotlinecesena/utilities/MathUtils.java
@@ -0,0 +1,12 @@
+package hotlinecesena.utilities;
+
+import javafx.geometry.Point2D;
+
+public class MathUtils {
+
+    public static final double SIN_45 = .707106;
+    
+    public static Point2D normalizeDiagonalMovement(final Point2D point) {
+        return point.multiply(SIN_45);
+    }
+}
