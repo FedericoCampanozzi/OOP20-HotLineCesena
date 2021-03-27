809c6bb3e706121615f89c50b082d853500fd7fd
Remisse <zeromuseg7@gmail.com>
2021-03-27 18:40:30 +0100

    Inventory handles reloading

--- app/src/main/java/hotlinecesena/model/inventory/Inventory.java
+++ app/src/main/java/hotlinecesena/model/inventory/Inventory.java
@@ -18,4 +18,6 @@ public interface Inventory {
     void reloadEquipped();
     
     boolean isReloading();
+    
+    void update(double timeElapsed);
 }
--- app/src/main/java/hotlinecesena/model/inventory/NaiveInventoryImpl.java
+++ app/src/main/java/hotlinecesena/model/inventory/NaiveInventoryImpl.java
@@ -1,32 +1,58 @@
 package hotlinecesena.model.inventory;
 
+import java.util.Map;
 import java.util.Optional;
 
 import hotlinecesena.model.entities.items.Item;
 
 public class NaiveInventoryImpl implements Inventory {
 
+    private static final double DEFAULT_RELOAD_TIME = 2.0;
+    private double reloadTimeRemaining = 0.0;
+    private Optional<Item> equippable = Optional.empty();
+    private Map<Item, Integer> stackables;
+
     @Override
     public void add(Item item) {
-        // TODO Auto-generated method stub
-
+//        if (isWeapon(item)) {
+//            if (this.equippable.isPresent()) {
+//                this.drop(this.equippable.get());
+//            }
+//            this.equippable = Optional.of(item);
+//        } else {
+//            
+//        }
+        final int oldQuantity = this.stackables.containsKey(item) ? this.stackables.get(item) : 0;
+        this.stackables.put(item, 1 + oldQuantity);
     }
 
     @Override
     public Optional<Item> getEquipped() {
-        return Optional.empty();
+        return this.equippable;
     }
 
     @Override
     public void reloadEquipped() {
-        // TODO Auto-generated method stub
-
+        if (!this.isReloading()) {
+            this.reloadTimeRemaining = DEFAULT_RELOAD_TIME;
+        }
     }
 
     @Override
     public boolean isReloading() {
-        // TODO Auto-generated method stub
-        return false;
+        return this.reloadTimeRemaining > 0.0;
+    }
+    
+    public void update(double timeElapsed) {
+        if (this.isReloading()) {
+            reloadTimeRemaining -= timeElapsed;
+            if (reloadTimeRemaining <= 0.0) {
+//                this.equippable.get().reload();
+            }
+        }
     }
 
+    private void drop(Item item) {
+        //TODO Remove from inventory and then add to DAL?
+    }
 }
