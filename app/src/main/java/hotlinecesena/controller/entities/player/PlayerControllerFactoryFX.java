b95658bcc4d0139ee284325b1c5bd0758d0c5014
Remisse <zeromuseg7@gmail.com>
2021-03-28 18:26:10 +0200

    Now instantiates view and camera

--- app/src/main/java/hotlinecesena/controller/entities/player/PlayerControllerFactoryFX.java
+++ app/src/main/java/hotlinecesena/controller/entities/player/PlayerControllerFactoryFX.java
@@ -1,34 +1,38 @@
 package hotlinecesena.controller.entities.player;
 
-import static hotlinecesena.model.entities.actors.player.PlayerAction.ATTACK;
-import static hotlinecesena.model.entities.actors.player.PlayerAction.MOVE_EAST;
-import static hotlinecesena.model.entities.actors.player.PlayerAction.MOVE_NORTH;
-import static hotlinecesena.model.entities.actors.player.PlayerAction.MOVE_SOUTH;
-import static hotlinecesena.model.entities.actors.player.PlayerAction.MOVE_WEST;
-import static hotlinecesena.model.entities.actors.player.PlayerAction.RELOAD;
+import static hotlinecesena.model.entities.actors.player.PlayerAction.*;
 
 import java.util.EnumMap;
 import java.util.Map;
 
+import hotlinecesena.controller.input.InputInterpreter;
 import hotlinecesena.controller.input.InputInterpreterImpl;
 import hotlinecesena.controller.input.InputListenerFX;
 import hotlinecesena.model.entities.actors.player.Player;
 import hotlinecesena.model.entities.actors.player.PlayerAction;
 import hotlinecesena.model.entities.actors.player.PlayerImpl;
 import hotlinecesena.model.inventory.NaiveInventoryImpl;
+import hotlinecesena.view.entities.Camera;
+import hotlinecesena.view.entities.CameraImpl;
+import hotlinecesena.view.entities.SpriteView;
+import hotlinecesena.view.entities.SpriteViewImpl;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.layout.Pane;
 
 public final class PlayerControllerFactoryFX implements PlayerControllerFactory {
     
     // TODO: All temporary, will replace with values given by DAL
+    private static final String SPRITE_NAME = "index.png";
     private static final double STARTING_ANGLE = 90;
-    private static final double STARTING_SPEED = 10;
+    private static final double STARTING_SPEED = 5000;
     private static final double MAX_HEALTH = 100;
     private static final Point2D STARTING_POS = Point2D.ZERO;
     private final Scene scene;
+    private final Pane pane;
     // TODO: Read bindings from file?
     private final Map<KeyCode, PlayerAction> keyBindings = new EnumMap<>(Map.of(
             KeyCode.W,             MOVE_NORTH,
@@ -41,8 +45,9 @@ public final class PlayerControllerFactoryFX implements PlayerControllerFactory 
             MouseButton.PRIMARY,   ATTACK
             ));
 
-    public PlayerControllerFactoryFX(final Scene scene) {
+    public PlayerControllerFactoryFX(final Scene scene, final Pane pane) {
         this.scene = scene;
+        this.pane = pane;
     }
 
     @Override
@@ -52,9 +57,14 @@ public final class PlayerControllerFactoryFX implements PlayerControllerFactory 
                 new NaiveInventoryImpl(),
                 Map.of()
                 );
-        final InputInterpreterImpl<KeyCode, MouseButton> input = new InputInterpreterImpl<>(
-                keyBindings, mouseBindings, new InputListenerFX(scene));
 
-        return new PlayerControllerFX(playerModel, null, input);
+        final SpriteView view = new SpriteViewImpl(new Image(SPRITE_NAME), pane);
+
+        final InputInterpreter<KeyCode, MouseButton> input = new InputInterpreterImpl<>(
+                keyBindings, mouseBindings, new InputListenerFX(scene, view));
+
+        final Camera camera = new CameraImpl(scene);
+
+        return new PlayerControllerFX(playerModel, view, input, camera);
     }
 }
