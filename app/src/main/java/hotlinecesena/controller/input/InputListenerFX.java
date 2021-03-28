d161bdb7a49fadb664adafadbf5646d954817f48
Remisse <zeromuseg7@gmail.com>
2021-03-28 18:28:45 +0200

    Process mouse coordinates based on sprite position

--- app/src/main/java/hotlinecesena/controller/input/InputListenerFX.java
+++ app/src/main/java/hotlinecesena/controller/input/InputListenerFX.java
@@ -6,10 +6,12 @@ import java.util.Set;
 import org.apache.commons.lang3.tuple.ImmutableTriple;
 import org.apache.commons.lang3.tuple.Triple;
 
+import hotlinecesena.view.entities.SpriteView;
 import javafx.geometry.Point2D;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 /**
  * 
@@ -18,20 +20,23 @@ import javafx.scene.input.MouseButton;
  */
 public final class InputListenerFX implements InputListener<KeyCode, MouseButton> {
 
+    private static final float DEADZONE = 65.0f;
     private final Set<KeyCode> keyboardInputs = new HashSet<>();
     private final Set<MouseButton> mouseInputs = new HashSet<>();
     private Point2D mouseMovement = Point2D.ZERO;
     private final Scene scene;
-    
-    public InputListenerFX(final Scene scene) {
+    private final SpriteView playerView;
+
+    public InputListenerFX(final Scene scene, final SpriteView view) {
         this.scene = scene;
+        this.playerView = view;
     }
 
     @Override
     public Triple<Set<KeyCode>, Set<MouseButton>, Point2D> deliverInputs() {
         this.listenForKeyInputs();
         this.listenForMouseButtons();
-        this.listenForMouseMovement();
+        this.listenForMouseMovement(playerView.getSpritePosition());
         return new ImmutableTriple<>(this.keyboardInputs, this.mouseInputs, this.mouseMovement);
     }
 
@@ -52,17 +57,31 @@ public final class InputListenerFX implements InputListener<KeyCode, MouseButton
             this.register(this.mouseInputs, e.getButton());
         });
     }
-    
-    private void listenForMouseMovement() {
+
+    private void listenForMouseMovement(final Point2D spritePosition) {
+        //Mouse moved, no buttons pressed
         scene.setOnMouseMoved(e -> {
-            this.mouseMovement = new Point2D(e.getSceneX(), e.getSceneY());
+            this.mouseMovement = this.retrieveMouseCoordinates(e, spritePosition);
+        });
+        //Mouse moved and buttons pressed
+        scene.setOnMouseDragged(e -> {
+            this.mouseMovement = this.retrieveMouseCoordinates(e, spritePosition);
         });
     }
-    
+
+    private Point2D retrieveMouseCoordinates(MouseEvent e, Point2D spritePosition) {
+        // Ignore mouse movements if too close to the player
+        if (spritePosition.distance(e.getSceneX(), e.getSceneY()) > DEADZONE) {
+            return new Point2D(e.getSceneX() - spritePosition.getX(),
+                               e.getSceneY() - spritePosition.getY());
+        }
+        return this.mouseMovement;
+    }
+
     private <T extends Enum<T>> void register(Set<T> field, T code) {
         field.add(code);
     }
-    
+
     private <T extends Enum<T>> void unregister(Set<T> field, T code) {
         if (field.contains(code)) {
             field.remove(code);
