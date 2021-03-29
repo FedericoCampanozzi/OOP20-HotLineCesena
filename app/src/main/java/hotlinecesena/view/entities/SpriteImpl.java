f95be8cab9fe8130b35d7cf726648f442b122357
Remisse <zeromuseg7@gmail.com>
2021-03-29 23:36:18 +0200

    Add methods

--- app/src/main/java/hotlinecesena/view/entities/SpriteImpl.java
+++ app/src/main/java/hotlinecesena/view/entities/SpriteImpl.java
@@ -7,37 +7,46 @@ import javafx.scene.layout.Pane;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
 
-public class SpriteViewImpl implements SpriteView {
+public class SpriteImpl implements Sprite {
 
     private static final double SCALE = 0.2; //TODO Temporary, GameController will pass this to the constructor
     private static final double SPEED_SCALE = 10;
-    private final ImageView image;
+    private final ImageView imageView;
     private final Rotate rotate;
     private final Translate trans;
 
-    public SpriteViewImpl(final Image img, final Pane pane) {
-        this.image = new ImageView(img);
-        image.setScaleX(SCALE);
-        image.setScaleY(SCALE);
+    public SpriteImpl(final Image img, final Pane pane) {
+        this.imageView = new ImageView(img);
+        imageView.setScaleX(SCALE);
+        imageView.setScaleY(SCALE);
         
         this.rotate = new Rotate();
         this.trans = new Translate();
-        this.image.getTransforms().addAll(rotate, trans);
-        pane.getChildren().add(image);
+        this.imageView.getTransforms().addAll(rotate, trans);
+        pane.getChildren().add(imageView);
     }
 
     @Override
-    public void update(final Point2D entityPos, final double entityAngle) {
-        rotate.setAngle(entityAngle);
-        rotate.setPivotX(entityPos.getX()*SPEED_SCALE + image.getImage().getWidth()/2);
-        rotate.setPivotY(entityPos.getY()*SPEED_SCALE + image.getImage().getHeight()/2);
+    public void updatePosition(final Point2D entityPos) {
+        rotate.setPivotX(entityPos.getX()*SPEED_SCALE + imageView.getImage().getWidth()/2);
+        rotate.setPivotY(entityPos.getY()*SPEED_SCALE + imageView.getImage().getHeight()/2);
         trans.setX(entityPos.getX()*SPEED_SCALE);
         trans.setY(entityPos.getY()*SPEED_SCALE);
     }
 
     @Override
+    public void updateRotation(final double entityAngle) {
+        rotate.setAngle(entityAngle);
+    }
+
+    @Override
+    public void updateImage(Image image) {
+        this.imageView.setImage(image);
+    }
+
+    @Override
     public Point2D getSpritePosition() {
-        final Point2D bounds = image.localToScene(0, 0);
+        final Point2D bounds = imageView.localToScene(0, 0);
         return new Point2D(bounds.getX(), bounds.getY());
     }
 }
