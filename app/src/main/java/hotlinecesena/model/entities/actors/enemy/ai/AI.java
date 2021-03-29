513f84717b8df0ab0f5fd1c4afed77b7a1a11e34
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-03-29 22:49:32 +0200

    Finalizing Enemy AI

--- app/src/main/java/hotlinecesena/model/entities/actors/enemy/ai/AI.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/enemy/ai/AI.java
@@ -1,9 +1,18 @@
 package hotlinecesena.model.entities.actors.enemy.ai;
 
-import hotlinecesena.model.entities.actors.DirectionList;
+import java.util.Set;
+
 import javafx.geometry.Point2D;
 
 public interface AI {
 
-    public DirectionList getNextMove(Point2D pos, boolean pursuit);
+    public void setEnemyPos (Point2D pos);
+    
+    public Point2D getNextMove(Point2D target, boolean pursuit, Set<Point2D> map);
+    
+    public double getRotation(Point2D target, boolean pursuit);
+    
+    public boolean isInPursuit(Point2D target, double noise);
+    
+    public boolean isShooting(Point2D target);
 }
--- app/src/main/java/hotlinecesena/model/entities/actors/enemy/ai/AIImpl.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/enemy/ai/AIImpl.java
@@ -1,43 +1,123 @@
 package hotlinecesena.model.entities.actors.enemy.ai;
 
+import java.util.Set;
+
+import hotlinecesena.controller.physics.EnemyPhysics;
 import hotlinecesena.model.entities.actors.DirectionList;
 import hotlinecesena.model.entities.actors.enemy.ai.strategy.*;
 import hotlinecesena.model.entities.actors.enemy.EnemyType;
 import javafx.geometry.Point2D;
 
 public class AIImpl implements AI{
+    
+    private final static int FIELD_OF_VIEW = 90;
+    private final static int NOISE_TRESHOLD = 10;
+    private final static int VISION_RADIUS = 3;
+    private final static int HEARDABLE_RADIUS = 5;
+    private final static double LOOK_NORTH = 90;
+    private final static double LOOK_EAST_NORTH = 45;
+    private final static double LOOK_EAST = 0;
+    private final static double LOOK_EAST_SOUTH = -45;
+    private final static double LOOK_SOUTH = -90;
+    private final static double LOOK_WEST_SOUTH = -135;
+    private final static double LOOK_WEST = 180;
+    private final static double LOOK_WEST_NORTH = 135;
 
-    private MovementStrategy nextMove;
+    private final MovementStrategy strategy;
+    private final Set<Point2D> wallSet;
+    private Point2D current;
+    private Point2D nextMove;
+    private double rotation;
 
-    public AIImpl(EnemyType type) {
-        this.nextMove = this.getStrategy(type);
+    public AIImpl(EnemyType type, double rotation, Set<Point2D> walls) {
+        this.strategy = this.getStrategy(type);
+        this.rotation = rotation;
+        this.wallSet = walls;
     }
 
     private MovementStrategy getStrategy(EnemyType type) {
-        MovementStrategy retval;
-
         switch(type) {
             case IDLE:
-                retval = new Idle();
-                break;
+                return new Idle();
             case RANDOM_MOVEMENT:
-                retval = new RandomMovement();
-                break;
+                return new RandomMovement();
             case PATROLLING:
-                retval = new Patrolling();
-                break;
+                return new Patrolling();
             default:
-                retval = null;
-                break;
-            
+                return null;
         }
+    }
+    
 
-        return retval;
+    private double rotationToDirection() {
+        if(this.nextMove.equals(DirectionList.NORTH.get())) {
+            return LOOK_NORTH;
+        } else if(this.nextMove.equals(DirectionList.EAST.get())) {
+            return LOOK_EAST;
+        } else if(this.nextMove.equals(DirectionList.SOUTH.get())) {
+            return LOOK_SOUTH;
+        } else if(this.nextMove.equals(DirectionList.WEST.get())) {
+            return LOOK_WEST;
+        } else if(this.nextMove.equals(new Point2D(-1,1))) {
+            return LOOK_WEST_NORTH;
+        } else if(this.nextMove.equals(new Point2D(1,1))) {
+            return LOOK_EAST_NORTH;
+        } else if(this.nextMove.equals(new Point2D(1,-1))) {
+            return LOOK_EAST_SOUTH;
+        } else if(this.nextMove.equals(new Point2D(-1,-1))) {
+            return LOOK_WEST_SOUTH;
+        } else {
+            return LOOK_NORTH;
+        }
+    }
+    
+    private double rotationToTarget(Point2D target) {
+        double distanceX, distanceY;
+        
+        distanceX = Math.abs(target.getX() - this.current.getX());
+        distanceY = Math.abs(target.getY() - this.current.getY());
+        
+        return Math.toDegrees(Math.atan2(this.current.getY() > target.getY() ? distanceY : -distanceY,
+                this.current.getX() > target.getX() ? -distanceX : distanceX));
+    }
+    
+    private boolean isInArea(Point2D target, int radius) {
+        return target.getX() >= this.current.getX() - radius && target.getX() <= this.current.getX() + radius
+                && target.getY() >= this.current.getY() - radius && target.getY() <= this.current.getY() + radius;
+    }
+    
+    private boolean inLineOfSight(Point2D target) {
+        return this.rotationToTarget(target) >= this.rotation - (FIELD_OF_VIEW / 2)
+                && this.rotationToTarget(target) <= this.rotation + (FIELD_OF_VIEW / 2)
+                && !EnemyPhysics.isWallInBetween(target, this.current, this.wallSet); 
     }
 
     @Override
-    public DirectionList getNextMove(Point2D pos, boolean pursuit) {
-        return this.nextMove.move(pos, pursuit);
+    public void setEnemyPos(Point2D pos) {
+        this.current = pos;
     }
 
+    @Override
+    public Point2D getNextMove(Point2D player, boolean pursuit, Set<Point2D> map) {
+        this.nextMove = this.strategy.move(this.current, player, pursuit, map);
+        return this.nextMove;
+    }
+
+    @Override
+    public double getRotation(Point2D target, boolean pursuit) {
+        this.rotation = !pursuit ? this.rotationToDirection() : this.rotationToTarget(target);
+        return this.rotation;
+    }
+
+    @Override
+    public boolean isInPursuit(Point2D target, double noise) {
+        return (noise / EnemyPhysics.distance(this.current, target) >= NOISE_TRESHOLD
+                    && this.isInArea(target, HEARDABLE_RADIUS))
+                || this.isShooting(target);
+    }
+
+    @Override
+    public boolean isShooting(Point2D target) {
+        return this.isInArea(target, VISION_RADIUS) && this.inLineOfSight(target);
+    }
 }
