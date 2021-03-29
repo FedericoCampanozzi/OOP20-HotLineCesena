e011fadc1c74031ff800f470286cf0e7e2ec656d
Remisse <zeromuseg7@gmail.com>
2021-03-29 23:25:32 +0200

    Add method to detach camera, update Javadoc

--- app/src/main/java/hotlinecesena/view/entities/Camera.java
+++ app/src/main/java/hotlinecesena/view/entities/Camera.java
@@ -11,14 +11,25 @@ import javafx.scene.layout.Pane;
 public interface Camera {
 
     /**
-     * Attaches this camera to a JavaFX pane.
+     * Attaches a {@link Translate} to a JavaFX {@link Pane} to simulate camera movement.
      * 
-     * @param pane the {@link Pane} which will be translated.
+     * @param pane the {@code Pane} which will be translated.
      */
     void attachToPane(Pane pane);
+    
+    /**
+     * Detaches this camera's {@link Translate} from a JavaFX {@link Pane}, if it was previously attached.
+     * 
+     * @param pane the {@code Pane} from which the {@code Translate} will be detached.
+     */
+    void detachFromPane(Pane pane);
 
     /**
      * Updates the camera position based on an entity's sprite coordinates.
+     * 
+     * @param spritePos the entity's sprite position on the Scene.
+     * @param deltaTime time elapsed since the last frame was rendered. Used for interpolating
+     * camera movement.
      */
     void update(Point2D spritePos, double deltaTime);
 }
