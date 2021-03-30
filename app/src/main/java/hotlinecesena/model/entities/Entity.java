45e8e0d05f8db93e134c0f76f4f370dcd0ac6c56
Remisse <zeromuseg7@gmail.com>
2021-03-30 21:36:14 +0200

    Extend Publisher

--- app/src/main/java/hotlinecesena/model/entities/Entity.java
+++ app/src/main/java/hotlinecesena/model/entities/Entity.java
@@ -1,5 +1,6 @@
 package hotlinecesena.model.entities;
 
+import hotlinecesena.model.events.Publisher;
 import javafx.geometry.Point2D;
 
 /**
@@ -7,14 +8,15 @@ import javafx.geometry.Point2D;
  * Represents a basic game entity to which a set of coordinates is associated.
  *
  */
-public interface Entity {
-    
+public interface Entity extends Publisher {
+
     /**
+     * Gets this entity's current coordinates on the game map.
      * 
-     * @return this entity's coordinates on the game map.
+     * @return this entity's coordinates.
      */
     Point2D getPosition();
-    
+
     /**
      * Gets the angle this actor is currently facing.
      * 
