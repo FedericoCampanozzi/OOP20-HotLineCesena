b54e2459c36ae84063676a08e1d4cac8c009d3a2
Remisse <zeromuseg7@gmail.com>
2021-04-01 18:15:19 +0200

    Minor tweaks

--- app/src/main/java/hotlinecesena/controller/entities/player/PlayerController.java
+++ app/src/main/java/hotlinecesena/controller/entities/player/PlayerController.java
@@ -1,10 +1,10 @@
 package hotlinecesena.controller.entities.player;
 
 import hotlinecesena.controller.Updatable;
-import hotlinecesena.view.Camera;
+import hotlinecesena.view.camera.CameraController;
 
 public interface PlayerController extends Updatable {
 
     // TODO To be removed when the GameController will be made to hold the camera
-    Camera getCamera();
+    CameraController getCamera();
 }
