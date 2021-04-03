693b3899062a70927c7bd4824baebe611d95ed57
Remisse <zeromuseg7@gmail.com>
2021-04-03 14:45:20 +0200

    Rename method

--- app/src/test/java/hotlinecesena/ActorModelTest.java
+++ app/src/test/java/hotlinecesena/ActorModelTest.java
@@ -57,7 +57,7 @@ class ActorModelTest {
     }
 
     @Test
-    private void actorCannotRestartReloadingWhileAlreadyReloading() {
+    private void actorCannotInitiateReloadingWhileAlreadyReloading() {
         
     }
 
