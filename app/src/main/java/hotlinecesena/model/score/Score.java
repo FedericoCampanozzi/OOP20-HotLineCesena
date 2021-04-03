477ce4dd08a4432fd8419a9f298933b3811fe85a
Remisse <zeromuseg7@gmail.com>
2021-04-03 17:27:25 +0200

    Redo Score implementation

--- app/src/main/java/hotlinecesena/model/score/Score.java
+++ app/src/main/java/hotlinecesena/model/score/Score.java
@@ -2,13 +2,9 @@ package hotlinecesena.model.score;
 
 import java.util.Map;
 
-import hotlinecesena.model.score.PartialScores.PartialType;
-
 public interface Score {
 
-    Map<PartialType, Integer> getPartialScores();
-    
+    Map<String, Integer> getPartialScores();
+
     int getTotalScore();
-    
-    void update(double timeElapsed);
 }
--- app/src/main/java/hotlinecesena/model/score/ScoreImpl.java
+++ app/src/main/java/hotlinecesena/model/score/ScoreImpl.java
@@ -1,65 +1,29 @@
 package hotlinecesena.model.score;
 
-import java.util.EnumMap;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.Set;
+import java.util.stream.Collectors;
 
-import com.google.common.eventbus.Subscribe;
-
-import hotlinecesena.model.entities.actors.player.Player;
-import hotlinecesena.model.events.AttackPerformedEvent;
 import hotlinecesena.model.events.Subscriber;
-import hotlinecesena.model.score.PartialScores.PartialType;
 
 public class ScoreImpl implements Score, Subscriber {
-    
-    private static final double TIME_CEILING = 300;
-    private double secondsElapsed = 0;
-    private int attacksPerformed = 0;
-    private int hits = 0;
-    private int killCount = 0;
+
+    private final Set<PartialScore> partials;
+
+    public ScoreImpl(final PartialScoreFactory factory) {
+        this.partials = factory.createAll();
+    }
 
     @Override
-    public Map<PartialType, Integer> getPartialScores() {
-//        return new EnumMap<>(Map.of(
-//                PartialScores.computeKillPartial(killCount, world.getEnemies().size(),
-//                PartialScores.computeTimePartial((int)secondsElapsed, TIME_CEILING),
-//                PartialScores.computeAccuracyPartial(hits, attacksPerformed))
-//                ));
-        return null;
+    public Map<String, Integer> getPartialScores() {
+        return this.partials.stream()
+                .collect(Collectors.toMap(PartialScore::getName, PartialScore::getPartialPoints));
     }
 
     @Override
     public int getTotalScore() {
-        return this.getPartialScores().entrySet()
-                .stream()
-                .mapToInt(Entry::getValue)
+        return this.partials.stream()
+                .mapToInt(PartialScore::getPartialPoints)
                 .sum();
     }
-
-    @Override
-    public void update(double timeElapsed) {
-        this.secondsElapsed += timeElapsed;
-    }
-    
-//   @Subscribe
-//   private void handleDeathEvent(DeathEvent e) {
-//       if (world.getEnemies().contains(e.getSource()) {
-//           this.killCount++;
-//       }
-//   }
-
-//    @Subscribe
-//    private void handleAttackEvent(AttackPerformedEvent e) {
-//        if (e.getSource() == player) {
-//            this.attacksPerformed++;
-//        }
-//    }
-    
-//   @Subscribe
-//   private void handleDamageReceivedEvent(DamageReceivedEvent e) {
-//       if (world.getEnemies().contains(e.getSource()) {
-//           this.hits++;
-//       }
-//   }
 }
