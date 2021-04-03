c93b4f0d6ca356549d02b00099c476bd69981524
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-03 13:22:25 +0200

    Now enemies actually move and can change pursuit state

--- app/src/main/java/hotlinecesena/model/entities/actors/enemy/Enemy.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/enemy/Enemy.java
@@ -2,12 +2,15 @@ package hotlinecesena.model.entities.actors.enemy;
 
 import java.util.Set;
 
+import hotlinecesena.model.entities.actors.Actor;
 import hotlinecesena.model.entities.actors.enemy.ai.AI;
 import javafx.geometry.Point2D;
 
-public interface Enemy {
+public interface Enemy extends Actor {
 
     public AI getAI();
+    
+    public void setIsInPursuit(boolean pursuit);
     
     public Set<Point2D> getWalkable();
     
