89ebf557a4b32dc32418f35d1b4fe1a3f36a5763
unknown <federico.campanozzi@gmail.com>
2021-04-05 19:18:30 +0200

    old classes left by mistake

--- app/src/main/java/hotlinecesena/utilities/Utilities.java
+++ app/src/main/java/hotlinecesena/utilities/Utilities.java
@@ -1,15 +1,24 @@
 package hotlinecesena.utilities;
 
 import javafx.geometry.BoundingBox;
+import javafx.geometry.Point2D;
 import javafx.util.Pair;
 
 public class Utilities {
 	
 	public static Pair<Integer,Integer> sumPair(Pair<Integer,Integer> p1, Pair<Integer, Integer> p2) {
-		return new Pair<>(p1.getKey() + p2.getKey(), p1.getValue() +p2.getValue());
+		return new Pair<>(p1.getKey() + p2.getKey(), p1.getValue() + p2.getValue());
+	}
+	
+	public static Pair<Integer,Integer> subPair(Pair<Integer,Integer> p1, Pair<Integer, Integer> p2) {
+		return new Pair<>(p1.getKey() - p2.getKey(), p1.getValue() - p2.getValue());
 	}
 	
 	public static boolean intersect(BoundingBox b1, BoundingBox b2) {
 		return b1.intersects(b2);
+	}
+	
+	public static Point2D convertPairToPoint2D(Pair<Integer,Integer> p, int tileSize) {
+		return new Point2D(tileSize * p.getKey(), tileSize * p.getValue());
 	}
 }
