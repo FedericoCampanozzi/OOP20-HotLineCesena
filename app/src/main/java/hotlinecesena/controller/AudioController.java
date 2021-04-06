9891783d82012c53ac19a97772e58185200564aa
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-06 22:03:48 +0200

    Cleaning up code and writing javadoc

--- app/src/main/java/hotlinecesena/controller/AudioController.java
+++ app/src/main/java/hotlinecesena/controller/AudioController.java
@@ -5,17 +5,22 @@ import java.util.Optional;
 import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import hotlinecesena.model.entities.Entity;
 import hotlinecesena.model.entities.actors.enemy.Enemy;
-import hotlinecesena.model.events.Subscriber;
 import hotlinecesena.view.loader.AudioType;
 import hotlinecesena.view.loader.ProxyAudio;
 import hotlinecesena.view.loader.SoundLoader;
 import javafx.scene.media.AudioClip;
 import javafx.scene.media.MediaPlayer;
 
-public class AudioController implements Subscriber{
+/**
+ * Models the controller audio that allow an easy
+ * access to the {@link ProxyAudio} by exposing
+ * few ways to directly play the needed sound with
+ * the correct volume settings
+ */
+public class AudioController {
 
     private final static int PERCENTAGE = 100;
-    private final static int ENEMY_SOUNDS = 10;
+    private final static int BACKGROUND = 10;
 
     private final SoundLoader loader;
     private final Optional<Entity> caller;
@@ -23,6 +28,11 @@ public class AudioController implements Subscriber{
     private MediaPlayer audio;
     private double volume;
 
+    /**
+     * Class constructor
+     * @param caller the entity that create a new instance
+     * @see Entity
+     */
     public AudioController(final Optional<Entity> caller) {
         this.loader = new ProxyAudio();
         this.caller = caller;
@@ -31,18 +41,35 @@ public class AudioController implements Subscriber{
         this.volumeSettings();
     }
 
+    /**
+     * Updates the volume of the audio that is
+     * already playing in the background
+     * @param value the new volume setting
+     */
     private void updateVolume(final double value) {
         if(this.audio != null) {
             this.audio.setVolume(value);
         }
     }
 
+    /**
+     * Calculates the volume based on the
+     * {@code Entity} that created this instance
+     * @see Entity
+     */
     private void volumeSettings() {
         this.volume /= PERCENTAGE;
         this.volume = this.caller.isPresent() && this.caller.get() instanceof Enemy ?
-                this.volume /  ENEMY_SOUNDS : this.volume;
+                this.volume /  BACKGROUND : this.volume;
     }
 
+    /**
+     * Sets the new volume for this instance
+     * of the {@code AudioController} and
+     * updates the volume if a {@code MediaPlayer}
+     * track is already playing
+     * @param value the new volume setting
+     */
     public void setVolume(final double value) {
         this.volume = value;
         this.volumeSettings();
@@ -50,14 +77,24 @@ public class AudioController implements Subscriber{
         this.updateVolume(this.volume);
     }
 
+    /**
+     * Plays the {@code AudioClip} of the audio file specified
+     * by its relative path
+     * @param type the path of the file that wants to
+     * be reproduced
+     */
     public void playAudioClip(final AudioType type) {
         this.clip = this.loader.getAudioClip(type);
         this.clip.play(this.volume);
     }
 
+    /**
+     * Plays the {@code MediaPlayer} of the audio file specified
+     * by its relative path
+     */
     public void playMusic() {
         this.audio = this.loader.getMediaPlayer(AudioType.BACKGROUND);
-        this.audio.setVolume(this.volume / PERCENTAGE);
-        this.audio.play();
+        this.audio.setVolume((this.volume / PERCENTAGE) / BACKGROUND);
+        this.audio.setAutoPlay(true);
     }
 }
