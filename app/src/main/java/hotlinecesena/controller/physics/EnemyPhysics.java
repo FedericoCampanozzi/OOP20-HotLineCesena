ddca31b8974846d84f2079908c7886803e64c7f4
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-06 13:18:56 +0200

    Completed implementation with DAL enemy and player instance

--- app/src/main/java/hotlinecesena/controller/physics/EnemyPhysics.java
+++ app/src/main/java/hotlinecesena/controller/physics/EnemyPhysics.java
@@ -2,12 +2,18 @@ package hotlinecesena.controller.physics;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import javafx.geometry.Point2D;
 
 public class EnemyPhysics {
     
-    public static boolean isMoveAllowed(Point2D current, Point2D next, Set<Point2D> map) {
-        Set<Point2D> allowedMoveSet = new HashSet<>() {/**
+    public static boolean isMoveAllowed(final Point2D current, final Point2D next, 
+            final Set<Point2D> map) {
+
+        final Set<Point2D> allowedMoveSet = new HashSet<>() {
+            /**
              * 
              */
             private static final long serialVersionUID = 1L;
@@ -15,30 +21,41 @@ public class EnemyPhysics {
         {
            addAll(map); 
         }};
-        
-        //allowedMoveSet.removeAll(enemyList.stream().map(e -> e.getEnemyPosition()).collect(Collectors.toSet()));
-        //allowedMoveSet.remove(player);
-        
+
+        allowedMoveSet.removeAll(JSONDataAccessLayer.getInstance()
+                .getEnemy()
+                .getEnemies()
+                .stream()
+                    .filter(e -> !e.getPosition().equals(current))
+                    .map(e -> e.getPosition())
+                    .collect(Collectors.toSet()));
+        allowedMoveSet.remove(JSONDataAccessLayer.getInstance()
+                .getPlayer()
+                .getPly()
+                    .getPosition());
+
         return allowedMoveSet.contains(sum(current, next));
     }
-    
-    public static boolean isWallInBetween(Point2D current, Point2D target, Set<Point2D> walls) {
+
+    public static boolean isWallInBetween(final Point2D current, final Point2D target,
+            final Set<Point2D> walls) {
+
         int x =(int) current.getX();
         int y =(int) current.getY();
         int distanceX, distanceY;
-        
+
         distanceX =(int) (current.getX() - target.getX());
         distanceY =(int) (current.getY() - target.getY());
-        
+
         distanceY = distanceY == 0 ? 1 : distanceY;
-        
+
         while((distanceX > 0 ? x > target.getX() : x < target.getX()) 
                 || (distanceY > 0 ? y > target.getY() : y < target.getY())) {
-            
+
             if(walls.contains(new Point2D(x,y))) {
                 return true;
             }
-            
+
             if(distanceX != 0) {
                 x = distanceX > 0 ? x-1 : x+1;
             }
@@ -47,15 +64,16 @@ public class EnemyPhysics {
             }
 
         }
-
         return false;
     }
-    
-    private static Point2D sum(Point2D arg1, Point2D arg2) {
-        return new Point2D((int)arg1.getX() + (int)arg2.getX(), (int)arg1.getY() + (int)arg2.getY());
+
+    private static Point2D sum(final Point2D arg1, final Point2D arg2) {
+        return new Point2D((int)arg1.getX() + (int)arg2.getX(),
+                (int)arg1.getY() + (int)arg2.getY());
     }
-    
-    public static int distance(Point2D arg1, Point2D arg2) {
-        return (int) (Math.abs(arg1.getX() - arg2.getX()) + Math.abs(arg1.getY() - arg2.getY()));
+
+    public static int distance(final Point2D arg1, final Point2D arg2) {
+        return (int) (Math.abs(arg1.getX() - arg2.getX()) +
+                Math.abs(arg1.getY() - arg2.getY()));
     }
 }
