cf5dbabf6581bfee8d324afab92bfa166fad7153
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-06 13:24:58 +0200

    Fixed implementation with new DAL and cleaning up code

--- app/src/main/java/hotlinecesena/controller/AudioController.java
+++ app/src/main/java/hotlinecesena/controller/AudioController.java
@@ -1,9 +1,8 @@
 package hotlinecesena.controller;
 
-import java.io.IOException;
 import java.util.Optional;
 
-import hotlinecesena.model.DALImpl;
+import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import hotlinecesena.model.entities.Entity;
 import hotlinecesena.model.entities.actors.enemy.Enemy;
 import hotlinecesena.model.events.Subscriber;
@@ -14,53 +13,48 @@ import javafx.scene.media.AudioClip;
 import javafx.scene.media.MediaPlayer;
 
 public class AudioController implements Subscriber{
-    
+
     private final static int PERCENTAGE = 100;
     private final static int ENEMY_SOUNDS = 10;
-    
+
     private final SoundLoader loader;
+    private final Optional<Entity> caller;
     private AudioClip clip;
     private MediaPlayer audio;
-    private Optional<Entity> caller;
     private double volume;
-        
-    public AudioController(Optional<Entity> caller) {
+
+    public AudioController(final Optional<Entity> caller) {
         this.loader = new ProxyAudio();
         this.caller = caller;
+        this.volume = JSONDataAccessLayer.getInstance().getSettings().getVolume();
 
-        try {
-            this.volume = DALImpl.getInstance().getIntegerSetting("volume");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
         this.volumeSettings();
     }
-    
-    private void updateVolume(double value) {
+
+    private void updateVolume(final double value) {
         if(this.audio != null) {
             this.audio.setVolume(value);
         }
     }
-    
+
     private void volumeSettings() {
         this.volume /= PERCENTAGE;
         this.volume = this.caller.isPresent() && this.caller.get() instanceof Enemy ?
                 this.volume /  ENEMY_SOUNDS : this.volume;
     }
-    
-    public void setVolume(double value) {
+
+    public void setVolume(final double value) {
         this.volume = value;
         this.volumeSettings();
-        
+
         this.updateVolume(this.volume);
     }
-    
-    public void playAudioClip(AudioType type) {
+
+    public void playAudioClip(final AudioType type) {
         this.clip = this.loader.getAudioClip(type);
         this.clip.play(this.volume);
     }
-    
+
     public void playMusic() {
         this.audio = this.loader.getMediaPlayer(AudioType.BACKGROUND);
         this.audio.setVolume(this.volume / PERCENTAGE);
