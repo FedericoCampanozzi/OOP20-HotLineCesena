fa7ac6ca2d1d37c08e387aa7d3305f645e091f3e
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-06 17:58:53 +0200

    Cleaning up code and writing javadoc

--- app/src/main/java/hotlinecesena/controller/physics/EnemyPhysics.java
+++ app/src/main/java/hotlinecesena/controller/physics/EnemyPhysics.java
@@ -5,10 +5,26 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
+import hotlinecesena.model.entities.actors.enemy.Enemy;
 import javafx.geometry.Point2D;
 
+/**
+ * Collection of static methods to check enemy physics
+ * @see Enemy
+ */
 public class EnemyPhysics {
-    
+
+    /**
+     * Calculates if the position that wants to be reached
+     * is walkable by the enemy, and checks if there are no
+     * other enemies, player or objects that could prevent
+     * the movement
+     * @param current the current position
+     * @param next the position that wants to be reached
+     * @param map the collections of points that are walkable
+     * by the enemy
+     * @return if the {@code next} position is walkable
+     */
     public static boolean isMoveAllowed(final Point2D current, final Point2D next, 
             final Set<Point2D> map) {
 
@@ -37,15 +53,25 @@ public class EnemyPhysics {
         return allowedMoveSet.contains(sum(current, next));
     }
 
+    /**
+     * Calculates if the {@code current} position has a clear
+     * sight to the {@code target} position by traversing the
+     * shortest path to each points and checking if any walls
+     * are in the way
+     * @param current the current position
+     * @param target the targeted position that wants to be checked
+     * @param walls the collections of all points that could prevent
+     * the two points to see each others
+     * @return if the {@code target} is visible from the {@code current}
+     * position
+     */
     public static boolean isWallInBetween(final Point2D current, final Point2D target,
             final Set<Point2D> walls) {
 
         int x =(int) current.getX();
         int y =(int) current.getY();
-        int distanceX, distanceY;
-
-        distanceX =(int) (current.getX() - target.getX());
-        distanceY =(int) (current.getY() - target.getY());
+        int distanceX =(int) (current.getX() - target.getX());
+        int distanceY =(int) (current.getY() - target.getY());
 
         distanceY = distanceY == 0 ? 1 : distanceY;
 
@@ -67,11 +93,23 @@ public class EnemyPhysics {
         return false;
     }
 
+    /**
+     * Calculates an integer sum between two points
+     * @param arg1 the first argument
+     * @param arg2 the second argument
+     * @return a new {@code Point2D}
+     */
     private static Point2D sum(final Point2D arg1, final Point2D arg2) {
         return new Point2D((int)arg1.getX() + (int)arg2.getX(),
                 (int)arg1.getY() + (int)arg2.getY());
     }
 
+    /**
+     * Calculates the distance between two points
+     * @param arg1 the first argument
+     * @param arg2 the second argument
+     * @return the integer value of the difference
+     */
     public static int distance(final Point2D arg1, final Point2D arg2) {
         return (int) (Math.abs(arg1.getX() - arg2.getX()) +
                 Math.abs(arg1.getY() - arg2.getY()));
