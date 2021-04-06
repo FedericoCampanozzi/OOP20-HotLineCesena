ab36aba02e7b34e3048716f488dd20e1e99b4032
Remisse <zeromuseg7@gmail.com>
2021-04-06 14:53:16 +0200

    Add basic starting inventory

--- app/src/main/java/hotlinecesena/model/entities/actors/player/PlayerFactoryImpl.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/player/PlayerFactoryImpl.java
@@ -4,6 +4,9 @@ import java.util.Map;
 import java.util.Objects;
 
 import hotlinecesena.model.entities.actors.ActorStatus;
+import hotlinecesena.model.entities.items.AmmunitionType;
+import hotlinecesena.model.entities.items.WeaponImpl;
+import hotlinecesena.model.entities.items.WeaponType;
 import hotlinecesena.model.inventory.Inventory;
 import hotlinecesena.model.inventory.NaiveInventoryImpl;
 import javafx.geometry.Point2D;
@@ -12,11 +15,13 @@ public final class PlayerFactoryImpl implements PlayerFactory {
 
     private static final Point2D STARTING_POS = Point2D.ZERO;
     private static final double STARTING_ANGLE = 270;
-    private static final double WIDTH = 100; //TODO To be tested
-    private static final double HEIGHT = 300;
+    private static final double WIDTH = 20; //TODO To be tested
+    private static final double HEIGHT = 10;
     private static final double SPEED = 450;
     private static final double MAX_HEALTH = 100;
-    private final Inventory inv = new NaiveInventoryImpl();
+    private final Inventory inv = new NaiveInventoryImpl(
+            new WeaponImpl(WeaponType.PISTOL), Map.of(
+                    AmmunitionType.PISTOL_AMMO, 30));
     private final Map<ActorStatus, Double> noise = Map.of(
             ActorStatus.IDLE, 0.0,
             ActorStatus.MOVING, 5.0,
