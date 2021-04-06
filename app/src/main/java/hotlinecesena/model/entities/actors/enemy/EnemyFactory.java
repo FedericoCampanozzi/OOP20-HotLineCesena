c8a3cc3b8f44888ab218dca3ebb2b4898ed41cd3
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-06 13:21:25 +0200

    Cleaning up code and writing javadoc

--- app/src/main/java/hotlinecesena/model/entities/actors/enemy/EnemyFactory.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/enemy/EnemyFactory.java
@@ -2,9 +2,30 @@ package hotlinecesena.model.entities.actors.enemy;
 
 import java.util.Set;
 
+import hotlinecesena.model.inventory.Inventory;
 import javafx.geometry.Point2D;
 
+/**
+ * Model a factory that generate enemies
+ */
 public interface EnemyFactory {
 
+    /**
+     * Returns a new specific enemy. Based on the {@code EnemyType}
+     * the enemy will be able to move around the game map, chase down
+     * the player and shoot the weapon in his {@code Inventory}
+     * @param pos the starting position
+     * @param type the type of movement that the enemy will inherit 
+     * @param walkable collections of points that are traversable by
+     * the enemy
+     * @param walls collections of all wall objects that could reduce
+     * enemy vision
+     * @return a new enemy implementation
+     * @see Enemy
+     * @see EnemyImpl
+     * @see EnemyType
+     * @see Inventory
+     */
     public Enemy getEnemy (Point2D pos, EnemyType type, Set<Point2D> walkable, Set<Point2D> walls);
+
 }
--- app/src/main/java/hotlinecesena/model/entities/actors/enemy/EnemyFactoryImpl.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/enemy/EnemyFactoryImpl.java
@@ -5,16 +5,20 @@ import java.util.Set;
 import hotlinecesena.model.inventory.NaiveInventoryImpl;
 import javafx.geometry.Point2D;
 
+/**
+ * Class that represent the generic factory implementation
+ */
 public class EnemyFactoryImpl implements EnemyFactory {
 
     @Override
-    public Enemy getEnemy(Point2D pos, EnemyType type, Set<Point2D> walkable, Set<Point2D> walls) {        
+    public Enemy getEnemy(final Point2D pos, final EnemyType type,
+            final Set<Point2D> walkable, final Set<Point2D> walls) {
+
         switch(type) {
             case BOSS:
-                return null;
+                return new EnemyImpl(pos, new NaiveInventoryImpl(), EnemyType.PATROLLING, walkable, walls);
             default:
                 return new EnemyImpl(pos, new NaiveInventoryImpl(), type, walkable, walls);
         }
     }
-
 }
