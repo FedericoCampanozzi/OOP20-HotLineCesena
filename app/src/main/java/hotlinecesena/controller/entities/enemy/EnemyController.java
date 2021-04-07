0351729879419dfa3138f0d0e9c47457ca809b32
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-07 17:38:56 +0200

    Now the controller needs a Sprite for each enemy

--- app/src/main/java/hotlinecesena/controller/entities/enemy/EnemyController.java
+++ app/src/main/java/hotlinecesena/controller/entities/enemy/EnemyController.java
@@ -11,29 +11,40 @@ import hotlinecesena.controller.Updatable;
 import hotlinecesena.model.entities.actors.ActorStatus;
 import hotlinecesena.model.entities.actors.enemy.Enemy;
 import hotlinecesena.model.entities.actors.player.Player;
+import hotlinecesena.model.events.DeathEvent;
 import hotlinecesena.model.events.MovementEvent;
 import hotlinecesena.model.events.RotationEvent;
 import hotlinecesena.model.events.Subscriber;
 import hotlinecesena.view.entities.Sprite;
+import hotlinecesena.view.loader.ImageType;
+import hotlinecesena.view.loader.ProxyImage;
+import hotlinecesena.view.loader.SceneType;
 
 public class EnemyController implements Updatable, Subscriber {
 
+    private final ProxyImage loader;
     private final Map<Enemy, Sprite> enemyMap;
     private final Player player;
 
-    public EnemyController(final List<Enemy> enemyList, final Sprite sprite, final Player player) {
+    public EnemyController(final List<Enemy> enemyList, final List<Sprite> sprite, final Player player) {
+        this.loader = new ProxyImage();
         this.enemyMap = new HashMap<>();
         this.player = player;
         this.initialize(enemyList, sprite);
     }
 
-    private void initialize(final List<Enemy> enemyList, final Sprite sprite) {
+    private void initialize(final List<Enemy> enemyList, final List<Sprite> sprite) {
+        if(enemyList.size() != sprite.size()) {
+            throw new IllegalArgumentException("Different lists size");
+        }
+
         enemyList.stream().forEach(e -> {
             e.register(this);
-            final Sprite enemySprite = sprite;
+            final Sprite enemySprite = sprite.get(0);
             enemySprite.updatePosition(e.getPosition());
             enemySprite.updateRotation(e.getAngle());
             this.enemyMap.put(e, enemySprite);
+            sprite.remove(0);
         });
     }
 
@@ -68,5 +79,11 @@ public class EnemyController implements Updatable, Subscriber {
     @Subscribe
     private void updateSpriteOnRotationEvent(final RotationEvent<Enemy> e) {
         this.enemyMap.get(e.getSource()).updateRotation(e.getNewAngle());
+    }
+
+    @Subscribe
+    private void onDeathEvent(final DeathEvent<Enemy> e) {
+        this.enemyMap.get(e.getSource()).updateImage(this.loader.getImage(SceneType.GAME, ImageType.ENEMY_1));
+        e.getSource().unregister(this);
     }
 }
