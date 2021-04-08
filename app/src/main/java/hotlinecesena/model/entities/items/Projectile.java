aa2f39d319be8556eb2f013225c7c3dfdb277c29
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2021-04-08 19:02:08 +0200

    Add getter for gripPane in WorldView and optimization of blenders in WorldView. Second implementation of projectiles

--- app/src/main/java/hotlinecesena/model/entities/items/Projectile.java
+++ app/src/main/java/hotlinecesena/model/entities/items/Projectile.java
@@ -1,16 +1,28 @@
 package hotlinecesena.model.entities.items;
 
 import java.util.Objects;
+import java.util.Optional;
 
 import hotlinecesena.model.entities.AbstractMovableEntity;
+import hotlinecesena.model.entities.actors.enemy.Enemy;
+import hotlinecesena.model.entities.actors.player.Player;
 import hotlinecesena.model.events.MovementEvent;
 import hotlinecesena.utilities.MathUtils;
 import javafx.geometry.Point2D;
 
 public class Projectile extends AbstractMovableEntity {
+	
+	public enum ProjectileStatus{
+		MOVING,
+		STOP;
+	}
+	
+	private ProjectileStatus projectileStatus = ProjectileStatus.MOVING;
+	private double damage;
 
-	protected Projectile(Point2D position, double angle, double width, double height, double speed) {
+	protected Projectile(Point2D position, double angle, double width, double height, double speed, final double damage) {
 		super(position, angle, width, height, speed);
+		this.damage = damage;
 	}
 
 	@Override
@@ -18,19 +30,27 @@ public class Projectile extends AbstractMovableEntity {
 		Objects.requireNonNull(direction);
         final Point2D oldPos = this.getPosition();
         final Point2D newPos = oldPos.add(direction.multiply(this.getSpeed()));
-        if (!this.hasHitEnemy(newPos) && !this.hasHitObstacle(newPos)) {
-            this.setPosition(newPos);
-            this.publish(new MovementEvent<>(this, newPos));
-        }
         if (this.hasHitObstacle(newPos)) {
-			// cancello il proiettile
+			this.projectileStatus = ProjectileStatus.STOP;
 		}
-        if(this.hasHitEnemy(newPos)) {
-        	// infliggo danni al nemico e cancello il proiettile
+        else {
+        	final Optional<Enemy> enemy = this.hasHitEnemy(newPos);
+        	if (enemy.isPresent()) {
+				enemy.get().takeDamage(this.damage);
+				this.projectileStatus = ProjectileStatus.STOP;
+			}
+        	else {
+        		final Optional<Player> player = this.hasHitPlayer(newPos);
+        		if (player.isPresent()) {
+					player.get().takeDamage(this.damage);
+					this.projectileStatus = ProjectileStatus.STOP;
+				}
+        		else {
+        			this.setPosition(newPos);
+                    this.publish(new MovementEvent<>(this, newPos));
+				}
+			}
         }
-        if (this.hasHitPlayer(newPos)) {
-			// infliggo danni al player e cancello il proiettile
-		}
     }
 	
 	@Override
@@ -38,12 +58,14 @@ public class Projectile extends AbstractMovableEntity {
 		return false;
 	}
 	
-	private boolean hasHitEnemy(final Point2D newPos) {
+	private Optional<Enemy> hasHitEnemy(final Point2D newPos) {
+        	
         return this.getGameMaster().getEnemy().getEnemies()
                 .stream()
-                .anyMatch(e -> MathUtils.isCollision(
+                .filter(e -> MathUtils.isCollision(
                         newPos, this.getWidth(), this.getHeight(),
-                        e.getPosition(), e.getWidth(), e.getHeight()));
+                        e.getPosition(), e.getWidth(), e.getHeight()))
+                .findFirst();
     }
 	
 	private boolean hasHitObstacle(final Point2D newPos) {
@@ -54,7 +76,15 @@ public class Projectile extends AbstractMovableEntity {
                         new Point2D(o.getMinX() + o.getWidth() / 2, o.getMinY() + o.getHeight() / 2), o.getWidth(), o.getHeight()));
     }
 	
-	private boolean hasHitPlayer(final Point2D newPos) {
-		return false;
+	private Optional<Player> hasHitPlayer(final Point2D newPos) {
+		Player player = getGameMaster().getPlayer().getPly();
+		if (MathUtils.isCollision(newPos, getWidth(), getHeight(), player.getPosition(), player.getWidth(), player.getHeight())) {
+			return Optional.of(player);
+		}
+		return Optional.empty();
+	}
+	
+	public ProjectileStatus getProjectileStatus() {
+		return this.projectileStatus;
 	}
 }
--- app/src/main/java/hotlinecesena/view/WorldView.java
+++ app/src/main/java/hotlinecesena/view/WorldView.java
@@ -1,7 +1,8 @@
 package hotlinecesena.view;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
-
+import java.util.Map.Entry;
 import hotlinecesena.controller.GameLoopController;
 import hotlinecesena.controller.entities.player.PlayerController;
 import hotlinecesena.controller.entities.player.PlayerControllerFactoryFX;
@@ -30,7 +31,11 @@ public class WorldView {
     DataWorldMap world = JSONDataAccessLayer.getInstance().getWorld();
     Map<Pair<Integer, Integer>, SymbolsType> worldMap = world.getWorldMap();
     private final GameLoopController gc = new GameLoopController();
-    //private SceneSwapper sceneSwapper = new SceneSwapper();
+    
+    private Map<Pair<Integer, Integer>, ImageView> enemiesPos = new LinkedHashMap<>();
+    private Map<Pair<Integer, Integer>, ImageView> itemsPos = new LinkedHashMap<>();
+    private Map<Pair<Integer, Integer>, ImageView> obstaclesPos = new LinkedHashMap<>();
+    private Map<Pair<Integer, Integer>, ImageView> playersPos = new LinkedHashMap<>();
 
     public WorldView(final Stage primaryStage) {
         this.primaryStage = primaryStage;
@@ -50,6 +55,12 @@ public class WorldView {
                 pickImage(col, row);
             }
         }
+        
+        addAllToWorldMap(itemsPos);
+        addAllToWorldMap(obstaclesPos);
+        addAllToWorldMap(enemiesPos);
+        addAllToWorldMap(playersPos);
+        
         primaryStage.setResizable(false);
         primaryStage.setWidth(1600);
         primaryStage.setHeight(900);
@@ -69,17 +80,24 @@ public class WorldView {
         gc.loop();
     }
     
-    private void pickImage(final int col, final int row) {
+    private void addAllToWorldMap(Map<Pair<Integer, Integer>, ImageView> positionsMap) {
+		for(Entry<Pair<Integer, Integer>, ImageView> elem : positionsMap.entrySet()) {
+			Group group = createBlendGroup(elem.getValue());
+			gridPane.add(group, elem.getKey().getKey(), elem.getKey().getValue());
+		}
+	}
+
+	private void pickImage(final int col, final int row) {
     	final char c = worldMap.get(new Pair<Integer, Integer> (world.getMinX() + col, world.getMinY() + row)).getDecotification();
     	if (c == 'W' || c == '_' || c == '.') {
-			createImage(c, col, row);
+			createSingleImage(c, col, row);
 		}
     	else {
-			blendImages(c, col, row);
+    		savePosition(c, col, row);
 		}
     }
 
-    private void createImage(final char c, final int col, final int row) {
+    private void createSingleImage(final char c, final int col, final int row) {
         final ImageView tile = new ImageView();
         switch (c) {
 		case 'W':
@@ -90,46 +108,50 @@ public class WorldView {
 			break;
 		case '.':
 			tile.setImage(proxyImage.getImage(SceneType.GAME, ImageType.FLOOR));
-			break;
+			break;	
 		}
         tile.setFitHeight(TILE_SIZE);
         tile.setFitWidth(TILE_SIZE);
         gridPane.add(tile, col, row);
     }
     
-    private void blendImages(final char symbol, final int col, final int row) {
-    	final ImageView top = new ImageView();
-    	final ImageView bottom = new ImageView(proxyImage.getImage(SceneType.GAME, ImageType.FLOOR));
-    	switch (symbol) {
+    private void savePosition(char c, int col, int row) {
+    	switch (c) {
 		case 'P':
-			top.setImage(proxyImage.getImage(SceneType.GAME, ImageType.PLAYER));
+			this.playersPos.put(new Pair<Integer, Integer>(col, row), new ImageView(proxyImage.getImage(SceneType.GAME, ImageType.PLAYER)));
 			break;
 		case 'E':
-			top.setImage(proxyImage.getImage(SceneType.GAME, ImageType.ENEMY_1));
+			this.enemiesPos.put(new Pair<Integer, Integer>(col, row), new ImageView(proxyImage.getImage(SceneType.GAME, ImageType.ENEMY_1)));
 			break;
 		case 'M':
-			top.setImage(proxyImage.getImage(SceneType.GAME, ImageType.MEDKIT));
-			break;
+			this.itemsPos.put(new Pair<Integer, Integer>(col, row), new ImageView(proxyImage.getImage(SceneType.GAME, ImageType.MEDKIT)));
+			break;	
 		case 'A':
-			top.setImage(proxyImage.getImage(SceneType.GAME, ImageType.AMMO_PISTOL));
+			this.itemsPos.put(new Pair<Integer, Integer>(col, row), new ImageView(proxyImage.getImage(SceneType.GAME, ImageType.AMMO_PISTOL)));
 			break;
 		case 'O':
-			top.setImage(proxyImage.getImage(SceneType.GAME, ImageType.BOX));
+			this.obstaclesPos.put(new Pair<Integer, Integer>(col, row), new ImageView(proxyImage.getImage(SceneType.GAME, ImageType.BOX)));
 			break;
-		}
-    	top.setFitHeight(TILE_SIZE);
-    	top.setFitWidth(TILE_SIZE);
-    	bottom.setFitHeight(TILE_SIZE);
+    	}
+    }
+
+    private Group createBlendGroup(final ImageView imageView) {
+    	ImageView bottom = new ImageView(proxyImage.getImage(SceneType.GAME, ImageType.FLOOR));
+        ImageView top = imageView;
+        bottom.setFitHeight(TILE_SIZE);
         bottom.setFitWidth(TILE_SIZE);
-    	top.setBlendMode(BlendMode.SRC_OVER);
-    	Group blend = new Group(
+        top.setFitHeight(TILE_SIZE);
+        top.setFitWidth(TILE_SIZE);
+        top.setBlendMode(BlendMode.SRC_OVER);
+        Group blend = new Group(
     			bottom,
     			top
     	);
-		gridPane.add(blend, col, row);
+        return blend;
     }
     
     public GridPane getGridPane() {
     	return this.gridPane;
     }
+    
 }
