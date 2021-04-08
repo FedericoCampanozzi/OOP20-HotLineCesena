a3348d0434344873f48f1f2094853a28c0858802
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2021-04-08 16:59:10 +0200

    Fix sound control

--- app/src/main/java/hotlinecesena/controller/menu/StartMenuController.java
+++ app/src/main/java/hotlinecesena/controller/menu/StartMenuController.java
@@ -5,18 +5,15 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
+import hotlinecesena.controller.AudioController;
 import hotlinecesena.controller.WorldController;
 import hotlinecesena.utilities.SceneSwapper;
-import hotlinecesena.view.loader.AudioType;
-import hotlinecesena.view.loader.ProxyAudio;
 
 public class StartMenuController implements Initializable{
 	
@@ -28,29 +25,23 @@ public class StartMenuController implements Initializable{
 	private Button exitButton;
 	
 	private static SceneSwapper sceneSwapper = new SceneSwapper();
-	private MediaPlayer mediaPlayer;
-	ProxyAudio proxyAudio = new ProxyAudio();
+	AudioController audioController = new AudioController(Optional.empty());
 	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		mediaPlayer = proxyAudio.getMediaPlayer(AudioType.BACKGROUND);
-		mediaPlayer.play();
-		mediaPlayer.seek(Duration.ZERO);
+		audioController.playMusic();
 	}
 	
 	public void newGameClick(final ActionEvent event) throws IOException {
-		mediaPlayer.stop();
 		final Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         new WorldController(primaryStage);
 	}
 	
 	public void optionsClick(final ActionEvent event) throws IOException {
-		mediaPlayer.stop();
 		sceneSwapper.changeScene("OptionsView.fxml", event);
 	}
 	
 	public void exitClick(final ActionEvent event) throws IOException {
-		mediaPlayer.stop();
 		System.exit(0);
 	}
 }
--- app/src/main/java/hotlinecesena/view/WorldView.java
+++ app/src/main/java/hotlinecesena/view/WorldView.java
@@ -1,6 +1,5 @@
 package hotlinecesena.view;
 
-import java.io.IOException;
 import java.util.Map;
 
 import hotlinecesena.controller.GameLoopController;
@@ -9,17 +8,13 @@ import hotlinecesena.controller.entities.player.PlayerControllerFactoryFX;
 import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import hotlinecesena.model.dataccesslayer.SymbolsType;
 import hotlinecesena.model.dataccesslayer.datastructure.DataWorldMap;
-import hotlinecesena.utilities.SceneSwapper;
 import hotlinecesena.view.loader.ImageType;
 import hotlinecesena.view.loader.ProxyImage;
 import hotlinecesena.view.loader.SceneType;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.effect.BlendMode;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -31,7 +26,6 @@ public class WorldView {
     private final Stage primaryStage;
     private BorderPane borderPane;
     private final GridPane gridPane = new GridPane();
-    private final SceneSwapper sceneSwapper = new SceneSwapper();
     ProxyImage proxyImage = new ProxyImage();
     DataWorldMap world = JSONDataAccessLayer.getInstance().getWorld();
     Map<Pair<Integer, Integer>, SymbolsType> worldMap = world.getWorldMap();
@@ -62,20 +56,6 @@ public class WorldView {
         primaryStage.setX(0);
         primaryStage.setY(0);
 
-        primaryStage.getScene().setOnKeyReleased(new EventHandler<KeyEvent>() {
-
-            @Override
-            public void handle(final KeyEvent event) {
-                if (event.getCode() == KeyCode.ESCAPE) {
-                    try {
-                        sceneSwapper.newStageWithScene("PauseView.fxml");
-                    } catch (final IOException e) {
-                        // TODO Auto-generated catch block
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
         final PlayerController pc = new PlayerControllerFactoryFX(primaryStage.getScene(), gridPane).createPlayerController();
         gc.addMethodToUpdate(pc.getUpdateMethod());
         System.out.println("Obstacles: ");
@@ -147,5 +127,9 @@ public class WorldView {
     			top
     	);
 		gridPane.add(blend, col, row);
+    }
+    
+    public GridPane getGridPane() {
+    	return this.gridPane;
     }
 }
