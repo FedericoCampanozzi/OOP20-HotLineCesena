cd6e42a8a2e33f2292e484d06ae72f7f2331bf50
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-10 17:55:50 +0200

    Changed implementation to fit one enemy per controller

--- app/src/main/java/hotlinecesena/controller/entities/enemy/EnemyController.java
+++ app/src/main/java/hotlinecesena/controller/entities/enemy/EnemyController.java
@@ -8,6 +8,8 @@ import java.util.function.Consumer;
 import com.google.common.eventbus.Subscribe;
 
 import hotlinecesena.controller.Updatable;
+import hotlinecesena.controller.physics.EnemyPhysics;
+import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import hotlinecesena.model.entities.actors.ActorStatus;
 import hotlinecesena.model.entities.actors.enemy.Enemy;
 import hotlinecesena.model.entities.actors.player.Player;
@@ -19,6 +21,7 @@ import hotlinecesena.view.entities.Sprite;
 import hotlinecesena.view.loader.ImageType;
 import hotlinecesena.view.loader.ProxyImage;
 import hotlinecesena.view.loader.SceneType;
+import javafx.geometry.BoundingBox;
 
 /**
  * Controls the actions that each enemy
@@ -28,8 +31,10 @@ import hotlinecesena.view.loader.SceneType;
 public class EnemyController implements Updatable, Subscriber {
 
     private final ProxyImage loader;
-    private final Map<Enemy, Sprite> enemyMap;
+    private final Enemy enemy;
+    private final Sprite sprite;
     private final Player player;
+    private long lastTime;
 
     /**
      * Class constructor
@@ -39,13 +44,16 @@ public class EnemyController implements Updatable, Subscriber {
      * @see Player
      * @see Sprite
      */
-    public EnemyController(final List<Enemy> enemyList, final List<Sprite> sprite,
+    public EnemyController(final Enemy enemy, final Sprite sprite,
             final Player player) {
 
         this.loader = new ProxyImage();
-        this.enemyMap = new HashMap<>();
+        this.enemy = enemy;
+        this.enemy.register(this);
+        this.sprite = sprite;
         this.player = player;
-        this.initialize(enemyList, sprite);
+        //this.initialize(enemyList, sprite);
+        this.lastTime = System.currentTimeMillis();
     }
 
     /**
@@ -55,40 +63,41 @@ public class EnemyController implements Updatable, Subscriber {
      * @param sprite the collection of all the enemies images
      */
     private void initialize(final List<Enemy> enemyList, final List<Sprite> sprite) {
-        if(enemyList.size() != sprite.size()) {
-            throw new IllegalArgumentException("Different lists size");
-        }
-
-        enemyList.stream().forEach(e -> {
-            e.register(this);
-            final Sprite enemySprite = sprite.get(0);
-            enemySprite.updatePosition(e.getPosition());
-            enemySprite.updateRotation(e.getAngle());
-            this.enemyMap.put(e, enemySprite);
-            sprite.remove(0);
-        });
+//        if(enemyList.size() != sprite.size()) {
+//            throw new IllegalArgumentException("Different lists size");
+//        }
+//
+//        enemyList.stream().forEach(e -> {
+//            e.register(this);
+//            final Sprite enemySprite = sprite.get(0);
+//            enemySprite.updatePosition(e.getPosition());
+//            enemySprite.updateRotation(e.getAngle());
+//            this.enemyMap.put(e, enemySprite);
+//            sprite.remove(0);
+//        });
     }
 
     @Override
     public Consumer<Double> getUpdateMethod() {
         return deltaTime -> {
-            this.enemyMap.forEach((e, s) -> {
+            long current = System.currentTimeMillis();
                 // TODO to relief performance check if enemy is in range (es. cameraDimension) from the player to start beeing animated
-                if(!e.getActorStatus().equals(ActorStatus.DEAD)) {
-                    e.setIsInPursuit(e.getAI().isInPursuit(this.player.getPosition(), this.player.getNoiseRadius()));
+                if(!this.enemy.getActorStatus().equals(ActorStatus.DEAD)) {
+                    this.enemy.setIsInPursuit(this.enemy.getAI().isInPursuit(this.player.getPosition(), this.player.getNoiseRadius()));
 
-                    if(e.getAI().isShooting(this.player.getPosition())) {
-                        e.attack();
+                    if(this.enemy.getAI().isShooting(this.player.getPosition())) {
+                        this.enemy.attack();
                     } else {
-                        e.move(e.getAI().getNextMove(
+                        this.enemy.move(this.enemy.getAI().getNextMove(
                                 this.player.getPosition(),
-                                e.isChasingTarget(),
-                                e.getWalkable()));
+                                this.enemy.isChasingTarget(),
+                                this.enemy.getWalkable()).multiply(deltaTime));
                     }
 
-                    e.setAngle(e.getAI().getRotation(this.player.getPosition(), e.isChasingTarget()));
+                    this.enemy.setIsInPursuit(this.enemy.getAI().isInPursuit(this.player.getPosition(), this.player.getNoiseRadius()));
+                    this.enemy.setAngle(this.enemy.getAI().getRotation(this.player.getPosition(), this.enemy.isChasingTarget()));
+                    lastTime = System.currentTimeMillis();
                 }
-            });
         };
     }
 
@@ -99,7 +108,7 @@ public class EnemyController implements Updatable, Subscriber {
      */
     @Subscribe
     private void updateSpriteOnMoveEvent(final MovementEvent<Enemy> e) {
-        this.enemyMap.get(e.getSource()).updatePosition(e.getPosition());
+        this.sprite.updatePosition(e.getPosition());
     }
 
     /**
@@ -109,7 +118,7 @@ public class EnemyController implements Updatable, Subscriber {
      */
     @Subscribe
     private void updateSpriteOnRotationEvent(final RotationEvent<Enemy> e) {
-        this.enemyMap.get(e.getSource()).updateRotation(e.getNewAngle());
+        this.sprite.updateRotation(e.getNewAngle());
     }
 
     /**
@@ -119,7 +128,8 @@ public class EnemyController implements Updatable, Subscriber {
      */
     @Subscribe
     private void onDeathEvent(final DeathEvent<Enemy> e) {
-        this.enemyMap.get(e.getSource()).updateImage(this.loader.getImage(SceneType.GAME, ImageType.ENEMY_1));
+        this.sprite.updateImage(this.loader.getImage(SceneType.GAME, ImageType.ENEMY_1));
         e.getSource().unregister(this);
+        JSONDataAccessLayer.getInstance().getEnemy().getEnemies().remove(e.getSource());
     }
 }
