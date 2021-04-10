4b26b0e3b239a2d66ab3847d2f8839395865e368
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2021-04-11 00:41:04 +0200

    Cleaned WorldView, now istance of player and projectile controller are in WorldController

--- app/src/main/java/hotlinecesena/controller/WorldController.java
+++ app/src/main/java/hotlinecesena/controller/WorldController.java
@@ -1,17 +1,39 @@
 package hotlinecesena.controller;
 
+import hotlinecesena.controller.HUD.PlayerStatsController;
+import hotlinecesena.controller.entities.ProjectileController;
+import hotlinecesena.controller.entities.player.PlayerController;
+import hotlinecesena.controller.entities.player.PlayerControllerFactoryFX;
 import hotlinecesena.view.WorldView;
+import hotlinecesena.view.entities.SpriteImpl;
 import javafx.stage.Stage;
 
 public class WorldController{
 	
 	private final WorldView view;
 	private Stage primaryStage;
+	private final GameLoopController gameLoopController = new GameLoopController();
+    private PlayerController playerController;
+    private ProjectileController projectileController;
 	
 	public WorldController(Stage primaryStage){
 		this.primaryStage = primaryStage;
 		view = new WorldView(this.primaryStage);
 		view.start();
+		
+		playerController = new PlayerControllerFactoryFX(primaryStage.getScene(), view.getGridPane())
+                .createPlayerController(new SpriteImpl(view.getPlayersPos().getValue()));
+        gameLoopController.addMethodToUpdate(playerController.getUpdateMethod());
+        
+        projectileController = new ProjectileController(view);
+        gameLoopController.addMethodToUpdate(projectileController.getUpdateMethod());
+		
+        /*
+		PlayerStatsController playerStatsController = new PlayerStatsController(view);
+		gameLoopController.addMethodToUpdate(playerStatsController.getUpdateMethod());
+		*/
+		
+		gameLoopController.loop();
 	}
 	
 }
