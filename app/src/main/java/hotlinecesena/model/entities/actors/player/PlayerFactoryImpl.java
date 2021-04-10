7094b0728e4cd108a167db4f7162008e5ddec9e4
Remisse <zeromuseg7@gmail.com>
2021-04-10 18:37:28 +0200

    Tweak values

--- app/src/main/java/hotlinecesena/model/entities/actors/player/PlayerFactoryImpl.java
+++ app/src/main/java/hotlinecesena/model/entities/actors/player/PlayerFactoryImpl.java
@@ -3,7 +3,6 @@ package hotlinecesena.model.entities.actors.player;
 import java.util.Map;
 import java.util.Objects;
 
-import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import hotlinecesena.model.entities.actors.ActorStatus;
 import hotlinecesena.model.entities.items.AmmunitionType;
 import hotlinecesena.model.entities.items.WeaponImpl;
@@ -19,16 +18,16 @@ public final class PlayerFactoryImpl implements PlayerFactory {
 
     private static final Point2D STARTING_POS = Point2D.ZERO;
     private static final double STARTING_ANGLE = 270;
-    private static final double WIDTH = JSONDataAccessLayer.getInstance().getSettings().getTileSize(); //TODO To be tested
-    private static final double HEIGHT = JSONDataAccessLayer.getInstance().getSettings().getTileSize();
-    private static final double SPEED = 675;
+    private static final double WIDTH = 1; //TODO To be tested
+    private static final double HEIGHT = 1;
+    private static final double SPEED = 5;
     private static final double MAX_HEALTH = 100;
     private final Inventory inv = new NaiveInventoryImpl(
-            new WeaponImpl(WeaponType.PISTOL),
-            Map.of(AmmunitionType.PISTOL_AMMO, 30));
+            new WeaponImpl(WeaponType.RIFLE),
+            Map.of(AmmunitionType.RIFLE_AMMO, 90));
     private final Map<ActorStatus, Double> noise = Map.of(
             ActorStatus.IDLE, 0.0,
-            ActorStatus.MOVING, 5.0,
+            ActorStatus.MOVING, 2.0,
             ActorStatus.DEAD, 0.0
             );
 
