c94049295ec030cb0d7eab096e1623a386a6320b
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-11 17:53:10 +0200

    Added missing enums due to yesterday clean up

--- app/src/main/java/hotlinecesena/view/loader/ImageType.java
+++ app/src/main/java/hotlinecesena/view/loader/ImageType.java
@@ -1,38 +1,118 @@
 package hotlinecesena.view.loader;
 
 /**
- * Collections of all the possible images that can be displayed on the screen
+ * Collections of all the possible images that can be displayed on the screen.
  */
 public enum ImageType {
 
+    /**
+     * 
+     */
     PLAYER("player.png"),
 
+    /**
+     * 
+     */
     PLAYER_DEAD("playerDead.png"),
 
+    /**
+     * 
+     */
     PLAYER_RIFLE("playerRifle.png"),
 
+    /**
+     * 
+     */
     PLAYER_PISTOL("playerRifle.png"),
 
+    /**
+     * 
+     */
     PLAYER_SHOTGUN("playerRifle.png"),
 
+    /**
+     * 
+     */
     ENEMY_1("enemy.png"),
 
+    /**
+     * 
+     */
     ENEMY_DEAD("enemyDead.png"),
 
+    /**
+     * 
+     */
     WALL("wall.png"),
 
+    /**
+     * 
+     */
     FLOOR("floor.png"),
 
+    /**
+     * 
+     */
     GRASS("grass.png"),
 
+    /**
+     * 
+     */
     MEDKIT("medkit.png"),
 
+    /**
+     * 
+     */
     BULLET("shoot.png"),
 
+    /**
+     * 
+     */
     BOX("box.png"),
 
-    AMMO_PISTOL(""),
-    
+    /**
+     * 
+     */
+    AMMO_PISTOL("ammoPistol.png"),
+
+    /**
+     * 
+     */
+    AMMO_RIFLE("ammoRifle.png"),
+
+    /**
+     * 
+     */
+    PISTOL("pistol.png"),
+
+    /**
+     * 
+     */
+    RIFLE("rifle.png"),
+
+    /**
+     * 
+     */
+    SHOTGUN("shotgun.png"),
+
+    /**
+     * 
+     */
+    ICON("icon.png"),
+
+    /**
+     * 
+     */
+    MENU("menu.png"),
+
+    /**
+     * 
+     */
+    STARTING_SCREEN("startingScreen.png"),
+
+    /**
+     * 
+     */
     BLANK("blank.png");
 
     private final String path;
@@ -41,7 +121,7 @@ public enum ImageType {
      * @param path the relative path to each image file
      * @see String 
      */
-    private ImageType(final String path) {
+    ImageType(final String path) {
         this.path = path;
     }
 
