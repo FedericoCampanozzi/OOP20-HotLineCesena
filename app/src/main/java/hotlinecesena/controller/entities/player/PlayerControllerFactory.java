5a8f253b5972e41199fe5391b42b26970f92f159
Remisse <zeromuseg7@gmail.com>
2021-04-11 18:07:10 +0200

    Add Javadoc

--- app/src/main/java/hotlinecesena/controller/entities/player/PlayerControllerFactory.java
+++ app/src/main/java/hotlinecesena/controller/entities/player/PlayerControllerFactory.java
@@ -2,7 +2,17 @@ package hotlinecesena.controller.entities.player;
 
 import hotlinecesena.view.entities.Sprite;
 
+/**
+ *
+ * Models a factory for {@link PlayerController}s.
+ *
+ */
 public interface PlayerControllerFactory {
 
+    /**
+     * Instantiates a new {@code PlayerController} with the given {@link Sprite}.
+     * @param sprite the player sprite.
+     * @return a new {@code PlayerController} with the given {@code Sprite}.
+     */
     PlayerController createPlayerController(Sprite sprite);
 }
