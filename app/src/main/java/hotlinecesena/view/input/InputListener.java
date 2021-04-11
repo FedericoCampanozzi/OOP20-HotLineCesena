19984f2fcda49e3ca2ba19820c614f574879140e
Remisse <zeromuseg7@gmail.com>
2021-04-11 20:49:38 +0200

    Make InputListener more flexible

--- app/src/main/java/hotlinecesena/view/input/InputListener.java
+++ app/src/main/java/hotlinecesena/view/input/InputListener.java
@@ -3,12 +3,16 @@ package hotlinecesena.view.input;
 import java.util.Set;
 
 import javafx.geometry.Point2D;
+import javafx.scene.Scene;
 import javafx.util.Pair;
 
 /**
  *
- * Listens for inputs from the View. Its implementations are bound to be View-specific, meaning
- * a new implementation will be needed when switching to a different graphics library.
+ * Listens for keyboard and mouse inputs from the View.
+ * <br>
+ * It is bound to be View-specific: slight modifications will need
+ * to be made to this interface's methods when switching to a different
+ * graphics library.
  *
  */
 public interface InputListener {
@@ -18,4 +22,20 @@ public interface InputListener {
      * @return a {@link Pair} containing sets of raw inputs.
      */
     Pair<Set<Enum<?>>, Point2D> deliverInputs();
+
+    /**
+     *
+     * Binds keyboard and mouse event handlers to the given {@link Scene}.
+     * @param node the JavaFX {@code Scene} to which the event handlers
+     * will be set.
+     */
+    void setEventHandlers(Scene scene);
+
+    /**
+     * Removes this listener's event handlers from the given {@link Scene},
+     * if it contains them.
+     * @param node the JavaFX {@code Scene} from which this listener's
+     * event handlers will be removed.
+     */
+    void removeEventHandlersFrom(Scene scene);
 }
