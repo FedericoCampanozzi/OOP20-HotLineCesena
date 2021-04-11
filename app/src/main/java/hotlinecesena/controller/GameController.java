6c76e8c8ec7f22db1d79766b1e279d46e30b935a
unknown <federico.campanozzi@gmail.com>
2021-04-11 17:45:34 +0200

    shift mission controller from GameController to WorldController and test it

--- app/src/main/java/hotlinecesena/controller/GameController.java
+++ app/src/main/java/hotlinecesena/controller/GameController.java
@@ -8,14 +8,6 @@ import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import hotlinecesena.utilities.SceneSwapper;
 
 public class GameController extends Application {
-
-	private final MissionController mc = new MissionController();
-	private final GameLoopController lc = new GameLoopController();
-	
-	public MissionController getMissionController() {
-		return this.mc;
-	}
-	
 	public static void main(String[] args) throws IOException {
 		JSONDataAccessLayer.getInstance();
 		launch(args);
@@ -29,13 +21,6 @@ public class GameController extends Application {
         primaryStage.setTitle("HotLine Cesena");
         primaryStage.setResizable(false);
         primaryStage.show();
-        generateMissions();
         new AudioEventController();
-        lc.loop();
-	}
-	
-	private void generateMissions() {
-        mc.addQuest("uccidi 3 nemici", () -> JSONDataAccessLayer.getInstance().getEnemy().getDeathEnemyCount() == 3);
-        mc.addQuest("uccidi tutti i nemici", () -> JSONDataAccessLayer.getInstance().getEnemy().getEnemies().isEmpty());
 	}
 }
