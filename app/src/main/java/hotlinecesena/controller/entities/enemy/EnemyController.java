c2dae4167583a78ed7cd0b19f9c3888b71f17e9b
Andrea Micheli <andrea.micheli3@studio.unibo.it>
2021-04-11 17:50:32 +0200

    Fixing implementation

--- app/src/main/java/hotlinecesena/controller/entities/enemy/EnemyController.java
+++ app/src/main/java/hotlinecesena/controller/entities/enemy/EnemyController.java
@@ -1,15 +1,10 @@
 package hotlinecesena.controller.entities.enemy;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.function.Consumer;
 
 import com.google.common.eventbus.Subscribe;
 
 import hotlinecesena.controller.Updatable;
-import hotlinecesena.controller.physics.EnemyPhysics;
-import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import hotlinecesena.model.entities.actors.ActorStatus;
 import hotlinecesena.model.entities.actors.enemy.Enemy;
 import hotlinecesena.model.entities.actors.player.Player;
@@ -21,26 +16,28 @@ import hotlinecesena.view.entities.Sprite;
 import hotlinecesena.view.loader.ImageType;
 import hotlinecesena.view.loader.ProxyImage;
 import hotlinecesena.view.loader.SceneType;
-import javafx.geometry.BoundingBox;
-import javafx.scene.image.Image;
 
 /**
  * Controls the actions that each enemy
  * will perform during the game loop and
- * updates their respective image
+ * updates their respective image.
+ * @see Enemy
  */
-public class EnemyController implements Updatable, Subscriber {
+public final class EnemyController implements Updatable, Subscriber {
+
+    private static final int UPDATED_INTERVAL = 350;
+    private static final int UPDATE_AFTER_DEATH = 5000;
 
     private final ProxyImage loader;
     private final Enemy enemy;
     private final Sprite sprite;
     private final Player player;
     private long lastTime;
-    private long timeToDeath;
+    private long timeAfterDeath;
 
     /**
-     * Class constructor
-     * @param enemyList the collection of all the enemies
+     * Class constructor.
+     * @param enemy the animated enemy
      * @param sprite the collection of all the enemies images
      * @param player the target of our enemies
      * @see Player
@@ -54,41 +51,22 @@ public class EnemyController implements Updatable, Subscriber {
         this.enemy.register(this);
         this.sprite = sprite;
         this.player = player;
-        //this.initialize(enemyList, sprite);
         this.lastTime = System.currentTimeMillis();
-    }
-
-    /**
-     * Initialize {@code enemyMap} by associating
-     * an enemy to an image
-     * @param enemyList the collection of all the enemies
-     * @param sprite the collection of all the enemies images
-     */
-    private void initialize(final List<Enemy> enemyList, final List<Sprite> sprite) {
-//        if(enemyList.size() != sprite.size()) {
-//            throw new IllegalArgumentException("Different lists size");
-//        }
-//
-//        enemyList.stream().forEach(e -> {
-//            e.register(this);
-//            final Sprite enemySprite = sprite.get(0);
-//            enemySprite.updatePosition(e.getPosition());
-//            enemySprite.updateRotation(e.getAngle());
-//            this.enemyMap.put(e, enemySprite);
-//            sprite.remove(0);
-//        });
     }
 
     @Override
     public Consumer<Double> getUpdateMethod() {
         return deltaTime -> {
             long current = System.currentTimeMillis();
-                // TODO to relief performance check if enemy is in range (es. cameraDimension) from the player to start beeing animated
-                if(!this.enemy.getActorStatus().equals(ActorStatus.DEAD) && current - lastTime > 350) {
-                    this.timeToDeath = current;
-                    this.enemy.setIsInPursuit(this.enemy.getAI().isInPursuit(this.player.getPosition(), this.player.getNoiseRadius()));
+                if (!this.enemy.getActorStatus().equals(ActorStatus.DEAD)
+                        && !this.player.getActorStatus().equals(ActorStatus.DEAD)
+                        && current - lastTime > UPDATED_INTERVAL) {
 
-                    if(this.enemy.getAI().isShooting(this.player.getPosition())) {
+                    this.timeAfterDeath = current;
+                    this.enemy.setIsInPursuit(true);
+                    this.enemy.update(deltaTime);
+
+                    if (this.enemy.getAI().isShooting(this.player.getPosition())) {
                         this.enemy.attack();
                     } else {
                         this.enemy.move(this.enemy.getAI().getNextMove(
@@ -101,8 +79,8 @@ public class EnemyController implements Updatable, Subscriber {
                     this.enemy.setAngle(this.enemy.getAI().getRotation(this.player.getPosition(), this.enemy.isChasingTarget()));
                     lastTime = System.currentTimeMillis();
                 }
-                
-                if(this.enemy.getActorStatus().equals(ActorStatus.DEAD) && current - timeToDeath > 5000) {
+
+                if (this.enemy.getActorStatus().equals(ActorStatus.DEAD) && current - timeAfterDeath > UPDATE_AFTER_DEATH) {
                     this.sprite.updateImage(this.loader.getImage(SceneType.GAME, ImageType.BLANK));
                 }
         };
@@ -110,7 +88,7 @@ public class EnemyController implements Updatable, Subscriber {
 
     /**
      * Event triggered every time an enemy moves
-     * and updates its image
+     * and updates its image.
      * @param e the entity that produce the event
      */
     @Subscribe
@@ -120,7 +98,7 @@ public class EnemyController implements Updatable, Subscriber {
 
     /**
      * Event triggered every time an enemy rotates
-     * and updates its image
+     * and updates its image.
      * @param e the entity that produce the event
      */
     @Subscribe
@@ -130,14 +108,13 @@ public class EnemyController implements Updatable, Subscriber {
 
     /**
      * Event triggered once an enemy dies and
-     * updates its image
+     * updates its image.
      * @param e the entity that produce the event
      */
     @Subscribe
     private void onDeathEvent(final DeathEvent<Enemy> e) {
         this.sprite.updateImage(this.loader.getImage(SceneType.GAME, ImageType.ENEMY_DEAD));
         e.getSource().unregister(this);
-        this.timeToDeath = System.currentTimeMillis();
-        //JSONDataAccessLayer.getInstance().getEnemy().getEnemies().remove(this.enemy);
+        this.timeAfterDeath = System.currentTimeMillis();
     }
 }
