112516649e594568dbd22b5a28d7aa97d64b0b88
Remisse <zeromuseg7@gmail.com>
2021-04-12 20:24:25 +0200

    Make camera implement Updatable

--- app/src/main/java/hotlinecesena/view/camera/CameraView.java
+++ app/src/main/java/hotlinecesena/view/camera/CameraView.java
@@ -1,7 +1,7 @@
 package hotlinecesena.view.camera;
 
+import hotlinecesena.controller.Updatable;
 import hotlinecesena.view.entities.Sprite;
-import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 import javafx.scene.transform.Translate;
 
@@ -10,11 +10,16 @@ import javafx.scene.transform.Translate;
  * Controller for JavaFX which follows the movements of a {@link Sprite}.
  *
  */
-public interface CameraView {
+public interface CameraView extends Updatable {
+
+    /**
+     * Binds this camera to a given {@link Sprite}.
+     * @param sprite the sprite to which this camera will be bound to.
+     */
+    void bindToSprite(Sprite sprite);
 
     /**
      * Attaches a {@link Translate} to a JavaFX {@link Pane} to simulate camera movement.
-     *
      * @param pane the {@code Pane} which will be translated.
      */
     void setPane(Pane pane);
@@ -22,13 +27,5 @@ public interface CameraView {
     /**
      * Detaches the {@link Translate} from this camera's current JavaFX {@link Pane}.
      */
-    void removePane();
-
-    /**
-     * Updates the {@link Translate} position based on the movements of a {@link Sprite}.
-     *
-     * @param spritePosition Sprite position on the view.
-     * @param deltaTime time elapsed since the last update.
-     */
-    void update(Point2D spritePosition, double deltaTime);
+    void removeCameraFromPane();
 }
