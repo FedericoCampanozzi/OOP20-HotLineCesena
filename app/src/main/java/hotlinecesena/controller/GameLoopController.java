9004cfc9e6ab12b605dddb6250a0a2152cf7b39d
unknown <federico.campanozzi@gmail.com>
2021-04-13 14:22:07 +0200

    add stop method

--- app/src/main/java/hotlinecesena/controller/GameLoopController.java
+++ app/src/main/java/hotlinecesena/controller/GameLoopController.java
@@ -1,38 +1,43 @@
 package hotlinecesena.controller;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.function.Consumer;
-
 import javafx.animation.AnimationTimer;
 
 public class GameLoopController {
 	private final Set<Consumer<Double>> methods = new HashSet<>();
 	private long lastTime;
-	
+	private AnimationTimer animationTimer;
+
 	public GameLoopController() {
 		lastTime = System.currentTimeMillis();
 	}
 
 	public void loop() {
-		new AnimationTimer() {
+		animationTimer = new AnimationTimer() {
 			@Override
 			public void handle(final long now) {
 				loopMethod();
 			}
-		}.start();
+		};
+
+		animationTimer.start();
 	}
 
 	private void loopMethod() {
 		final long current = System.currentTimeMillis();
-		final int elapsed = (int)(current - lastTime);		
-		for(final Consumer<Double> m : methods) {
-			m.accept((double)elapsed / 1000.0d);
+		final int elapsed = (int) (current - lastTime);
+		for (Consumer<Double> m : this.methods) {
+			m.accept((double) elapsed / 1000.0d);
 		}
 		this.lastTime = current;
 	}
-	
+
 	public void addMethodToUpdate(final Consumer<Double> m) {
 		methods.add(m);
+	}
+
+	public void stop() {
+		this.animationTimer.stop();
 	}
 }
