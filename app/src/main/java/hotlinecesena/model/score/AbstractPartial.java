98152c3e1e19ab5d72d378c51e3a7151302db0b0
Remisse <zeromuseg7@gmail.com>
2021-04-13 18:04:41 +0200

    Fix mess with generics and EventBus

--- app/src/main/java/hotlinecesena/model/score/AbstractPartial.java
+++ app/src/main/java/hotlinecesena/model/score/AbstractPartial.java
@@ -6,33 +6,31 @@ import hotlinecesena.model.events.Subscriber;
 
 public abstract class AbstractPartial implements PartialScore, Subscriber {
 
-    private final String name;
-    private final int pointsPerUnit;
+    private final PartialType type;
 
-    protected AbstractPartial(final String name, final int points) {
-        this.name = name;
-        pointsPerUnit = points;
+    protected AbstractPartial(final PartialType type) {
+        this.type = type;
     }
 
     @Override
-    public final String getName() {
-        return name;
+    public final PartialType getType() {
+        return type;
     }
 
     @Override
     public final int getPartialPoints() {
-        return pointsPerUnit * this.getFactor();
+        return (int) (type.getPointsPerUnit() * this.formula());
     }
 
     /**
      * Template method that depends on formula().
      */
     @Override
-    public final int getFactor() {
+    public final double getFactor() {
         return this.formula();
     }
 
-    protected abstract int formula();
+    protected abstract double formula();
 
     protected final DataAccessLayer getGameMaster() {
         return JSONDataAccessLayer.getInstance();
