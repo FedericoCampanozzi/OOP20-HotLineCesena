233b45fd31f8a635c3bd864c25a73d7e7997c7c8
Remisse <zeromuseg7@gmail.com>
2021-04-14 14:15:55 +0200

    Change time format, add more conditions

--- app/src/main/java/hotlinecesena/model/score/partials/TimeStrategy.java
+++ app/src/main/java/hotlinecesena/model/score/partials/TimeStrategy.java
@@ -1,6 +1,7 @@
 package hotlinecesena.model.score.partials;
 
 import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
+import hotlinecesena.model.entities.actors.ActorStatus;
 
 public final class TimeStrategy implements PartialStrategy {
 
@@ -19,15 +20,23 @@ public final class TimeStrategy implements PartialStrategy {
                 - JSONDataAccessLayer.getInstance().getWorld().getMinX() + 1;
         final long mapHeight = JSONDataAccessLayer.getInstance().getWorld().getMaxY()
                 - JSONDataAccessLayer.getInstance().getWorld().getMinY() + 1;
-        timeCeiling = (mapWidth + mapHeight) / 2;
+        timeCeiling = (mapWidth + mapHeight) / 3;
     }
 
     @Override
     public int applyFormula() {
-        totalTime = (System.currentTimeMillis() - start) / 1000;
-        return (int) (basePoints * (timeCeiling <= totalTime ? 0 : timeCeiling - totalTime));
+        totalTime = System.currentTimeMillis() - start;
+        final long totalSeconds = totalTime / 1000;
+        return (int) (basePoints * (
+                timeCeiling <= totalSeconds
+                && JSONDataAccessLayer.getInstance().getPlayer().getPly().getActorStatus() != ActorStatus.DEAD ? 0
+                        : timeCeiling - totalSeconds
+                ));
     }
 
+    /**
+     * Return time in milliseconds.
+     */
     @Override
     public int getRelevantFactor() {
         return (int) totalTime;
