32392022f311097105daf737f0327a081b8cdbe5
unknown <federico.campanozzi@gmail.com>
2021-04-15 17:25:54 +0200

    remove map data from MiniMapController and add it to DataWorldMap

--- app/src/main/java/hotlinecesena/model/dataccesslayer/datastructure/DataWorldMap.java
+++ app/src/main/java/hotlinecesena/model/dataccesslayer/datastructure/DataWorldMap.java
@@ -3,6 +3,10 @@ package hotlinecesena.model.dataccesslayer.datastructure;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.util.Pair;
 import org.apache.commons.io.FileUtils;
 import hotlinecesena.controller.generator.BaseRoomsGeneratorFactory;
@@ -11,12 +15,16 @@ import hotlinecesena.controller.generator.WorldGeneratorBuilder;
 import hotlinecesena.model.dataccesslayer.AbstractData;
 import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import hotlinecesena.model.dataccesslayer.SymbolsType;
+import hotlinecesena.utilities.Utilities;
 
 public class DataWorldMap extends AbstractData {
 
 	private Map<Pair<Integer, Integer>, SymbolsType> worldMap;
 	private int xMin, xMax, yMin, yMax;
 	private boolean keyObj;
+	
+	private WritableImage writtableMiniMap;
+	private Pair<Integer, Integer> oldPlyPos;
 	
 	public DataWorldMap(DataJSONSettings settings) throws IOException {
 		WorldGeneratorBuilder sgwb = new RectangularWorldGeneratorBuilder()
@@ -42,7 +50,40 @@ public class DataWorldMap extends AbstractData {
 		this.yMin = sgwb.getMinY();
 		this.yMax = sgwb.getMaxY();
 		this.keyObj = sgwb.isKeyObjectPresent();
+		
+		int width =  settings.getPixelSize() * (getMaxX() - getMinX() + 1);
+		int height = settings.getPixelSize() * (getMaxY() - getMinY() + 1);
+		
+		writtableMiniMap = new WritableImage(width, height);
+		PixelWriter pw = writtableMiniMap.getPixelWriter();
+		
+		this.worldMap.entrySet().stream()
+			.forEach(itm->{
+				java.awt.Color color;
+				if(	itm.getValue().equals(SymbolsType.VOID) || 
+					itm.getValue().equals(SymbolsType.WALKABLE) ||
+					itm.getValue().equals(SymbolsType.PLAYER) || 
+					itm.getValue().equals(SymbolsType.WALL)){
+					color = itm.getValue().getMiniMapColor();
+				} else {
+					color = SymbolsType.WALKABLE.getMiniMapColor();
+				}
+				
+				pw.setColor(itm.getKey().getKey()- getMinX(), itm.getKey().getValue()- getMinY(), Utilities.convertColor(color));
+		});
+		
+		//oldPlyPos = Utilities.convertPoint2DToPair(JSONDataAccessLayer.getInstance().getPlayer().getPly().getPosition());
+		oldPlyPos = new Pair<>(0,0);
+		
 		write();
+	}
+	
+	public ImageView getImageVIewUpdated() {
+		PixelWriter pw = writtableMiniMap.getPixelWriter();
+		pw.setColor(oldPlyPos.getKey()- getMinX(), oldPlyPos.getValue()- getMinY(), Utilities.convertColor(SymbolsType.WALKABLE.getMiniMapColor()));
+		oldPlyPos = Utilities.convertPoint2DToPair(JSONDataAccessLayer.getInstance().getPlayer().getPly().getPosition());
+		pw.setColor(oldPlyPos.getKey()- getMinX(), oldPlyPos.getValue()- getMinY(), Utilities.convertColor(SymbolsType.PLAYER.getMiniMapColor()));
+		return new ImageView(writtableMiniMap);
 	}
 	
 	@Override
