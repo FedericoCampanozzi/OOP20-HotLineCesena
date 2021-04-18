834a2882ad24d5dbc9720a851dd1adc394e36191
Remisse <zeromuseg7@gmail.com>
2021-04-18 17:27:40 +0200

    Camera now follows mouse movement too

--- app/src/main/java/hotlinecesena/view/camera/CameraViewImpl.java
+++ app/src/main/java/hotlinecesena/view/camera/CameraViewImpl.java
@@ -5,8 +5,10 @@ import java.util.function.Consumer;
 
 import javax.annotation.Nonnull;
 
+import hotlinecesena.model.dataccesslayer.JSONDataAccessLayer;
 import hotlinecesena.utilities.MathUtils;
 import hotlinecesena.view.entities.Sprite;
+import hotlinecesena.view.input.InputListener;
 import javafx.geometry.Point2D;
 import javafx.scene.layout.Pane;
 import javafx.scene.transform.Translate;
@@ -17,11 +19,13 @@ import javafx.scene.transform.Translate;
  */
 public final class CameraViewImpl implements CameraView {
 
-    private static final double HUD_HEIGHT = 402; //TODO Gotta retrieve this from DAL
+    private static final double HUD_HEIGHT = JSONDataAccessLayer.getInstance().getSettings().getBotHudHeight();
     private static final double ACCEL = 30.0;
     private static final double SHARPNESS = 0.2;
+    private static final double MOUSE_LEEWAY = 0.002;
     private Pane pane;
     private Sprite sprite;
+    private final InputListener listener;
     private final Translate paneTranslate = new Translate();
 
     /**
@@ -29,8 +33,9 @@ public final class CameraViewImpl implements CameraView {
      * @param sprite the {@code Sprite} to which this camera will be bound to.
      * @throws NullPointerException if the given sprite is null.
      */
-    public CameraViewImpl(@Nonnull final Sprite sprite) {
+    public CameraViewImpl(@Nonnull final Sprite sprite, @Nonnull final InputListener listener) {
         this.bindToSprite(sprite);
+        this.listener = listener;
     }
 
     /**
@@ -59,7 +64,7 @@ public final class CameraViewImpl implements CameraView {
 
     /**
      * @implSpec Updates the {@link Translate} position based on the movements of
-     * the attached {@link Sprite}.
+     * the attached {@link Sprite} and the distance between Sprite and cursor.
      * @throws IllegalStateException if this camera is not attached to a pane
      * or is not bound to a sprite.
      */
@@ -71,11 +76,16 @@ public final class CameraViewImpl implements CameraView {
             }
             final double blend = MathUtils.blend(SHARPNESS, ACCEL, deltaTime);
             final Point2D currentPos = new Point2D(-paneTranslate.getX(), -paneTranslate.getY());
+            // Difference between mouse coordinates and sprite coordinates
+            final Point2D spriteMouseDistance = listener.deliverInputs().getValue().subtract(
+                    sprite.getPositionRelativeToScene());
             final Point2D newPos = MathUtils.lerp(
                     currentPos,
-                    sprite.getPositionRelativeToParent().subtract(
-                            pane.getScene().getWidth() / 2,
-                            (pane.getScene().getHeight() - HUD_HEIGHT) / 2),
+                    sprite.getPositionRelativeToParent()
+                    .subtract(pane.getScene().getWidth() / 2,
+                            (pane.getScene().getHeight() - HUD_HEIGHT) / 2)
+                    //Move camera away from the sprite based on the cursor's position
+                    .add(spriteMouseDistance.multiply(MOUSE_LEEWAY)),
                     blend);
             paneTranslate.setX(-newPos.getX());
             paneTranslate.setY(-newPos.getY());
