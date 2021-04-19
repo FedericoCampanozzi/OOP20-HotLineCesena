fff1b8b124b3520879a71d3b41fab6aba9cc50d7
Remisse <zeromuseg7@gmail.com>
2021-04-19 14:12:24 +0200

    Improve tests

--- app/src/test/java/hotlinecesena/InterpreterPlayerTest.java
+++ app/src/test/java/hotlinecesena/InterpreterPlayerTest.java
@@ -1,16 +1,8 @@
 package hotlinecesena;
 
-import static hotlinecesena.controller.entities.player.PlayerAction.ATTACK;
-import static hotlinecesena.controller.entities.player.PlayerAction.MOVE_EAST;
-import static hotlinecesena.controller.entities.player.PlayerAction.MOVE_NORTH;
-import static hotlinecesena.controller.entities.player.PlayerAction.MOVE_SOUTH;
-import static hotlinecesena.controller.entities.player.PlayerAction.MOVE_WEST;
-import static hotlinecesena.controller.entities.player.PlayerAction.RELOAD;
-import static hotlinecesena.controller.entities.player.PlayerAction.USE;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.in;
 import static org.hamcrest.Matchers.is;
 
@@ -25,10 +17,12 @@ import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.testfx.framework.junit5.Start;
 
-import hotlinecesena.controller.entities.player.Command;
 import hotlinecesena.controller.entities.player.InputInterpreter;
 import hotlinecesena.controller.entities.player.InputInterpreterImpl;
-import hotlinecesena.controller.entities.player.PlayerAction;
+import hotlinecesena.controller.entities.player.command.Command;
+import hotlinecesena.controller.entities.player.command.MoveCommand;
+import hotlinecesena.controller.entities.player.command.RotateCommand;
+import hotlinecesena.model.entities.actors.Direction;
 import hotlinecesena.model.entities.actors.DirectionList;
 import hotlinecesena.model.entities.actors.player.Player;
 import hotlinecesena.model.entities.actors.player.PlayerImpl;
@@ -55,21 +49,32 @@ class InterpreterPlayerTest {
     private static final double DELTA_TIME = 1.0;
     private Player player;
     private InputInterpreter interpreter;
-    private final Map<Enum<?>, PlayerAction> bindings = Map.of(
-            KeyCode.W,             MOVE_NORTH,
-            KeyCode.S,             MOVE_SOUTH,
-            KeyCode.D,             MOVE_EAST,
-            KeyCode.A,             MOVE_WEST,
-            KeyCode.R,             RELOAD,
-            KeyCode.E,             USE,
-            MouseButton.PRIMARY,   ATTACK
+    private final Map<Enum<?>, String> bindings = Map.of(
+            KeyCode.W,             "move_north",
+            KeyCode.S,             "move_south",
+            KeyCode.D,             "move_east",
+            KeyCode.A,             "move_west",
+            KeyCode.R,             "reload",
+            KeyCode.E,             "use",
+            MouseButton.PRIMARY,   "attack"
+            );
+    private final Map<String, Direction> movementActions = Map.of(
+            "move_north",   DirectionList.NORTH,
+            "move_south",   DirectionList.SOUTH,
+            "move_east",    DirectionList.EAST,
+            "move_west",    DirectionList.WEST
+            );
+    private final Map<String, Command> otherActions = Map.of(
+            "attack",       Player::attack,
+            "reload",       Player::reload,
+            "use",          Player::use
             );
 
     @Start
     private void setup() {
         player = new PlayerImpl(Point2D.ZERO, ANGLE, WIDTH, HEIGHT, SPEED, MAX_HP, new NaiveInventoryImpl(), Map.of(),
                 List.of(), List.of(), Map.of(), Map.of());
-        interpreter = new InputInterpreterImpl(bindings);
+        interpreter = new InputInterpreterImpl(bindings, movementActions, otherActions);
     }
 
     @BeforeEach
@@ -81,37 +86,35 @@ class InterpreterPlayerTest {
     void deliverNothingWhenReceivingNoInputs() {
         final Pair<Set<Enum<?>>, Point2D> inputs = new Pair<>(Set.of(), Point2D.ZERO);
         final Collection<Command> commands = interpreter.interpret(inputs, SPRITE_POS, DELTA_TIME);
-        assertThat(commands, empty());
+        assertThat(commands, is(empty()));
     }
 
     @Test
     void deliverNothingWhenReceivingUnboundInputs() {
         final Pair<Set<Enum<?>>, Point2D> inputs = new Pair<>(Set.of(KeyCode.J), Point2D.ZERO);
         final Collection<Command> commands = interpreter.interpret(inputs, SPRITE_POS, DELTA_TIME);
-        assertThat(commands, empty());
+        assertThat(commands, is(empty()));
     }
 
     @Test
-    void deliverAttack() {
+    void deliverMouseCommand() {
         final Pair<Set<Enum<?>>, Point2D> inputs = new Pair<>(Set.of(MouseButton.PRIMARY), Point2D.ZERO);
         final Collection<Command> commands = interpreter.interpret(inputs, SPRITE_POS, DELTA_TIME);
-        assertThat(PlayerAction.ATTACK.getCommand().get(), is(in(commands)));
+        assertThat(otherActions.get(bindings.get(MouseButton.PRIMARY)), is(in(commands)));
     }
 
     @Test
-    void deliverUse() {
+    void deliverKeyCommand() {
         final Pair<Set<Enum<?>>, Point2D> inputs = new Pair<>(Set.of(KeyCode.E), Point2D.ZERO);
         final Collection<Command> commands = interpreter.interpret(inputs, SPRITE_POS, DELTA_TIME);
-        assertThat(PlayerAction.USE.getCommand().get(), is(in(commands)));
+        assertThat(otherActions.get(bindings.get(KeyCode.E)), is(in(commands)));
     }
 
     @Test
     void deliverMovement() {
         final Pair<Set<Enum<?>>, Point2D> inputs = new Pair<>(Set.of(KeyCode.W), Point2D.ZERO);
         final Collection<Command> commands = interpreter.interpret(inputs, SPRITE_POS, DELTA_TIME);
-        assertThat(commands, hasSize(1));
-        commands.forEach(c -> c.execute(player));
-        assertThat(player.getPosition(), is(DirectionList.NORTH.get().multiply(SPEED).multiply(DELTA_TIME)));
+        assertThat(commands.stream().anyMatch(MoveCommand.class::isInstance), is(true));
     }
 
     @Test
@@ -120,7 +123,7 @@ class InterpreterPlayerTest {
         final double angle = MathUtils.mouseToDegrees(mouseCoords);
         final Pair<Set<Enum<?>>, Point2D> inputs = new Pair<>(Set.of(), mouseCoords);
         final Collection<Command> commands = interpreter.interpret(inputs, SPRITE_POS, DELTA_TIME);
-        assertThat(commands, hasSize(1));
+        assertThat(commands.stream().anyMatch(RotateCommand.class::isInstance), is(true));
         commands.forEach(c -> c.execute(player));
         assertThat(player.getAngle(), is(equalTo(angle)));
     }
